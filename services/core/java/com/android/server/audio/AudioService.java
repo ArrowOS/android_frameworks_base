af5992ef0f28d366077b878fd55322bbfa6daa88
Pawit Pornkitprasan <p.pawit@gmail.com>
2015-11-02 00:02:36 -0500

    Option to use volume keys to control media volume anytime (2/2)
    Squashed with:
    * Fix forcing media stream control on tablet
    * There's option volume_keys_control_ring_stream (Volume keys control ringtone
      volume / If on, volume keys control ringtone volume. If off, volume keys
      control media volume.) in Settings which is also available on tablets but it
      doesn't work on these devices (I didn't find reported issue).
      As I like this option and think it may be even more wanted on media devices
      such as tablets, here's a fix.
    
    PureNexus edits:
    * Modified to force media stream when toggled on, ring/notification stream when off
    
    ABC edits:
    * Use existing content resolver variable
    * Use one void for the setting getter
    
    Change-Id: I8c0c02a9205c1accdb5404183b7fda9a0eab749b

--- services/core/java/com/android/server/audio/AudioService.java
+++ services/core/java/com/android/server/audio/AudioService.java
@@ -592,6 +592,7 @@ public class AudioService extends IAudioService.Stub
     private AudioManagerInternal.RingerModeDelegate mRingerModeDelegate;
     private VolumePolicy mVolumePolicy = VolumePolicy.DEFAULT;
     private long mLoweredFromNormalToVibrateTime;
+    private boolean mVolumeKeysControlMediaStream;
 
     // Array of Uids of valid accessibility services to check if caller is one of them
     private int[] mAccessibilityServiceUids;
@@ -1271,6 +1272,8 @@ public class AudioService extends IAudioService.Stub
             updateRingerModeAffectedStreams();
             readDockAudioSettings(cr);
             sendEncodedSurroundMode(cr, "readPersistedSettings");
+
+            setVolumeKeysControlMediaStream();
         }
 
         mMuteAffectedStreams = System.getIntForUser(cr,
@@ -3939,10 +3942,16 @@ public class AudioService extends IAudioService.Stub
                     if (DEBUG_VOL)
                         Log.v(TAG, "getActiveStreamType: Forcing STREAM_MUSIC stream active");
                     return AudioSystem.STREAM_MUSIC;
+                } else {
+                    if (mVolumeKeysControlMediaStream) {
+                        if (DEBUG_VOL)
+                            Log.v(TAG, "getActiveStreamType: Forcing STREAM_MUSIC b/c user selected");
+                        return AudioSystem.STREAM_MUSIC;
                     } else {
                         if (DEBUG_VOL)
                             Log.v(TAG, "getActiveStreamType: Forcing STREAM_RING b/c default");
                         return AudioSystem.STREAM_RING;
+                    }
                 }
             } else if (isAfMusicActiveRecently(0)) {
                 if (DEBUG_VOL)
@@ -3971,9 +3980,15 @@ public class AudioService extends IAudioService.Stub
                     if (DEBUG_VOL) Log.v(TAG, "getActiveStreamType: forcing STREAM_MUSIC");
                     return AudioSystem.STREAM_MUSIC;
                 } else {
-                    if (DEBUG_VOL) Log.v(TAG,
-                            "getActiveStreamType: using STREAM_NOTIFICATION as default");
-                    return AudioSystem.STREAM_NOTIFICATION;
+                    if (mVolumeKeysControlMediaStream) {
+                        if (DEBUG_VOL)
+                            Log.v(TAG, "getActiveStreamType: Forcing STREAM_MUSIC b/c user selected");
+                        return AudioSystem.STREAM_MUSIC;
+                    } else {
+                        if (DEBUG_VOL) Log.v(TAG,
+                                "getActiveStreamType: using STREAM_NOTIFICATION as default");
+                        return AudioSystem.STREAM_NOTIFICATION;
+                    }
                 }
             }
             break;
@@ -5146,6 +5161,8 @@ public class AudioService extends IAudioService.Stub
                     Settings.Global.ENCODED_SURROUND_OUTPUT_AUTO);
             mContentResolver.registerContentObserver(Settings.Global.getUriFor(
                     Settings.Global.ENCODED_SURROUND_OUTPUT), false, this);
+            mContentResolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.VOLUME_KEYS_CONTROL_MEDIA_STREAM), false, this);
         }
 
         @Override
@@ -5166,6 +5183,8 @@ public class AudioService extends IAudioService.Stub
                 readDockAudioSettings(mContentResolver);
                 updateMasterMono(mContentResolver);
                 updateEncodedSurroundOutput();
+
+                setVolumeKeysControlMediaStream();
             }
         }
 
@@ -5194,6 +5213,12 @@ public class AudioService extends IAudioService.Stub
                 mEncodedSurroundMode = newSurroundMode;
             }
         }
+    }
+
+    private void setVolumeKeysControlMediaStream() {
+        mVolumeKeysControlMediaStream = Settings.System.getIntForUser(mContentResolver,
+                Settings.System.VOLUME_KEYS_CONTROL_MEDIA_STREAM, 0,
+                UserHandle.USER_CURRENT) == 1;
     }
 
     // must be called synchronized on mConnectedDevices
