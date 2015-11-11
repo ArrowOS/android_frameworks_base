32264f181d7ae4acd9f5549755e357ef383b77fb
Peter Schiller <peterschiller@google.com>
2015-11-11 12:54:14 -0800

    SystemUI: Adjusted animation timing
    Change-Id: I7f001abe0c97167ef9bdf83b3371b0875fd1c59f

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -5925,7 +5925,7 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
         if (!mNotificationPanel.isFullyCollapsed()) {
             // close the shade if it was open
             animateCollapsePanels(CommandQueue.FLAG_EXCLUDE_RECENTS_PANEL, true /* force */,
-                    true /* delayed */);
+                    true /* delayed */, NotificationPanelView.SPEED_UP_FACTOR_CLICKED);
             visibilityChanged(false);
 
             return true;
