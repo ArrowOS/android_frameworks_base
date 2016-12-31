0b60e9bdc73957eaf8f0869c8e8877bc2cdbd79a
bigrushdog <randall.rushing@gmail.com>
2016-12-31 13:24:50 -0800

    base: API to restart SystemUI without root
    Change-Id: I93843aed6fc61853e7bb6a8076a41880112a97bd

--- packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
@@ -94,6 +94,7 @@ public class CommandQueue extends IStatusBar.Stub {
     private static final int MSG_SHOW_PINNING_TOAST_ENTER_EXIT = 45 << MSG_SHIFT;
     private static final int MSG_SHOW_PINNING_TOAST_ESCAPE     = 46 << MSG_SHIFT;
     private static final int MSG_TOGGLE_CAMERA_FLASH           = 47 << MSG_SHIFT;
+    private static final int MSG_RESTART_UI                    = 48 << MSG_SHIFT;
 
     public static final int FLAG_EXCLUDE_NONE = 0;
     public static final int FLAG_EXCLUDE_SEARCH_PANEL = 1 << 0;
@@ -167,6 +168,7 @@ public class CommandQueue extends IStatusBar.Stub {
         default void onFingerprintError(String error) { }
         default void hideFingerprintDialog() { }
         default void toggleCameraFlash() { }
+        default void restartUI() { }
     }
 
     @VisibleForTesting
@@ -560,6 +562,13 @@ public class CommandQueue extends IStatusBar.Stub {
         }
     }
 
+    public void restartUI() {
+        synchronized (mLock) {
+            mHandler.removeMessages(MSG_RESTART_UI);
+            mHandler.obtainMessage(MSG_RESTART_UI).sendToTarget();
+        }
+    }
+
     private final class H extends Handler {
         private H(Looper l) {
             super(l);
@@ -809,6 +818,11 @@ public class CommandQueue extends IStatusBar.Stub {
                 case MSG_TOGGLE_CAMERA_FLASH:
                     for (int i = 0; i < mCallbacks.size(); i++) {
                         mCallbacks.get(i).toggleCameraFlash();
+                    }
+                    break;
+                case MSG_RESTART_UI:
+                    for (int i = 0; i < mCallbacks.size(); i++) {
+                        mCallbacks.get(i).restartUI();
                     }
                     break;
             }
