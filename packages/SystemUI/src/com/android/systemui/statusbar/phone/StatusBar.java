f0151795d34e6013bd5b80644cc3afcfc9578231
bigrushdog <randall.rushing@gmail.com>
2016-12-31 13:24:50 -0800

    base: API to restart SystemUI without root
    Change-Id: I4ce4762bee82d40daf49bacc846786bc9a3d938f

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -86,6 +86,7 @@ import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
 import android.os.PowerManager;
+import android.os.Process;
 import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.os.SystemClock;
@@ -4449,6 +4450,11 @@ public class StatusBar extends SystemUI implements DemoMode,
             mCommandQueue.recomputeDisableFlags(mDisplayId, true);
         }
         updateHideIconsForBouncer(true /* animate */);
+    }
+
+    public void restartUI() {
+        Log.d(TAG, "StatusBar API restartUI! Commiting suicide! Goodbye cruel world!");
+        Process.killProcess(Process.myPid());
     }
 
     protected void toggleKeyboardShortcuts(int deviceId) {
