dab99776641ad1cfedd26f3ceeb1ccc1ab125eb5
maxwen <max.weninger@gmail.com>
2017-02-25 17:31:05 +0100

    base: SystemUI: OmniJaws qs tile integration
    Change-Id: I4a00255a362298820f9f7997b3089e135358e0f6
    
    OmniJaws: Fix More Settings Intent
    
    Change-Id: I982f09d6ba5beadb5c38eb52029eff1b95cec04c
    
    base: qs weather: be nice to people without OmniJaws
    
    if OmniJaws is not available or disabled show an informative
    Toast. Also change the default tile image to show also some sun :)
    
    Change-Id: Id1216dfc2aa5a9cf86e1b6accaf354f25c5541fd
    
    base: SystemUI: remove force weather update bits
    
    force updates have been removed from service API
    
    Change-Id: I183bb1a3990d28211d99ff024f9faf59faa6ac4b
    
    base: SystemUI: use new OmniJaws error broadcasts
    
    Change-Id: I3c7da3843186897b7c93d477ca0696a39e0ada65
    
    base: SystemUI: dont hide weather forcast data on errors
    
    only show error in header
    see https://gerrit.omnirom.org/23116 for details
    
    Change-Id: Iaa2fa7b6e0d6638b142b19149c5b647d64f3b2e0
    
    base: SystemUI: dont show OmniJaws error that prominent
    
    instead just show a small * to indicate an error. In best case
    the update has been triggered on screen on anyway and will
    be updated soon
    
    Change-Id: I31446c8339e8093e41f9fc8a2788b251bcd3dced
    
    base: SystemUI: add dedicated TextView for OmniJaws error marker
    
    dont mess with the layout in the error case
    and fix proper layout handling for loooong city names
    
    Change-Id: I52919421cbb550e2a662e7df65118d4b03dcebf4
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -49,6 +49,7 @@ import com.android.systemui.qs.tiles.UiModeNightTile;
 import com.android.systemui.qs.tiles.UsbTetherTile;
 import com.android.systemui.qs.tiles.UserTile;
 import com.android.systemui.qs.tiles.SoundTile;
+import com.android.systemui.qs.tiles.WeatherTile;
 import com.android.systemui.qs.tiles.WifiTile;
 import com.android.systemui.qs.tiles.WorkModeTile;
 import com.android.systemui.util.leak.GarbageMonitor;
@@ -211,6 +212,8 @@ public class QSFactoryImpl implements QSFactory {
                 return mDataSwitchTileProvider.get();
             case "sound":
                 return mSoundTileProvider.get();
+            case "weather":
+                return new WeatherTile(mHost);
         }
 
         // Custom tiles
