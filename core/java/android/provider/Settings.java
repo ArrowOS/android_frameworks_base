3b7599a5d11d9ee2c2013c4eb30838b881d75220
maxwen <max.weninger@gmail.com>
2017-02-25 17:31:05 +0100

    base: Squashed import of OmniJaws integration
    This is a squash of the following:
    
    Author: maxwen <max.weninger@gmail.com>
    Date:   Sat Feb 25 17:31:05 2017 +0100
    base: SystemUI: OmniJaws qs tile integration
    
    Change-Id: I4a00255a362298820f9f7997b3089e135358e0f6
    
    OmniJaws: Fix More Settings Intent
    
    Change-Id: I982f09d6ba5beadb5c38eb52029eff1b95cec04c
    
    base: qs weather: be nice to people without OmniJaws
    
    if OmniJaws is not available or disabled show an informative
    Toast. Also change the default tile image to show also some sun :)
    
    Change-Id: Id1216dfc2aa5a9cf86e1b6accaf354f25c5541fd
    
    base: SystemUI: remove force weather update bits
    
    force updates have been removed from service API
    
    Change-Id: I183bb1a3990d28211d99ff024f9faf59faa6ac4b
    
    base: SystemUI: use new OmniJaws error broadcasts
    
    Change-Id: I3c7da3843186897b7c93d477ca0696a39e0ada65
    
    base: SystemUI: dont hide weather forcast data on errors
    
    only show error in header
    see https://gerrit.omnirom.org/23116 for details
    
    Change-Id: Iaa2fa7b6e0d6638b142b19149c5b647d64f3b2e0
    
    base: SystemUI: dont show OmniJaws error that prominent
    
    instead just show a small * to indicate an error. In best case
    the update has been triggered on screen on anyway and will
    be updated soon
    
    Change-Id: I31446c8339e8093e41f9fc8a2788b251bcd3dced
    
    base: SystemUI: add dedicated TextView for OmniJaws error marker
    
    dont mess with the layout in the error case
    and fix proper layout handling for loooong city names
    
    Change-Id: I52919421cbb550e2a662e7df65118d4b03dcebf4
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>
    
    Author: maxwen <max.weninger@gmail.com>
    Date:   Sun Oct 8 01:42:30 2017 +0200
    base: SystemUI: change disabled QS weather tile image
    
    Change-Id: I107ccabf655c7b9e099fac165cc46d88f009d3f7
    
    commit 5e01b70606ff1e8d946b7e5dec18cdda65f32c2c
    Author: maxwen <max.weninger@gmail.com>
    base: SystemUI: add tinting for OmniJaws provided QS images
    
    Change-Id: I3fb78564af673d4011b983d88a995dee63134352
    
    Author: maxwen <max.weninger@gmail.com>
    Date:   Tue Oct 10 01:42:20 2017 +0200
    base: SystemUI: weather: display wind direction as pinWheel and not degrees
    
    Change-Id: I4cfa994dd22d9a351aaf98c849a7718d3360c56c
    
    Author: maxwen <max.weninger@gmail.com>
    Date:   Wed Oct 18 00:38:11 2017 +0200
    base: SystemUI: add missing text tint in weather details
    
    Change-Id: I8f2bf9e0925fb338479d8502ca88bb90623cf908
    
    Author: maxwen <max.weninger@gmail.com>
    Date:   Wed Oct 25 16:54:07 2017 +0200
    base: SystemUI: no need for extra tint of qs weather tile
    
    framework is doing for us all needed
    
    Change-Id: Iab1ff2565bffa87f0806392664da9991f3606d1c
    
    Author: LorDClockaN <lordclockan@gmail.com>
    Date:   Mon Mar 6 22:32:58 2017 +0100
    WeatherTile: Start Google Weather on LongClick
    
    Change-Id: I049e026bcdcdea34ed61a07e6de953d91c6e57c1
    
    Author: SpiritCroc <spiritcroc@gmail.com>
    Date:   Wed Mar 8 20:26:35 2017 +0100
    Weather tile longclick: alternative weather apps when google weather not installed
    
    Change-Id: I2ea721a53d63dbd80edcc7a29e791f89c8cc0859
    
    Author: bigrushdog <randall.rushing@gmail.com>
    Date:   Tue Dec 19 20:30:58 2017 +0000
    base: Bringin UserContentObserver from DUtils
    
    * Squash of:
    DirtyUnicorns/android_external_DUtils@c250735
    DirtyUnicorns/android_external_DUtils@f9ad333
    DirtyUnicorns/android_external_DUtils@2d6ed52
    
    Change-Id: I368aa8bc84a33c69db21a02fbbb405bc2c187aad
    
    Author: maxwen <max.weninger@gmail.com>
    Date:   Thu Nov 2 11:52:10 2017 +0100
    base: SystemUI: filter service disabled broadcast for qs weather tile
    
    we handle disable extra. needed after revert of
    https://gerrit.omnirom.org/#/c/26541/
    
    Change-Id: I0b07fc438ae71145ef3b3a43f7b9fd5f7866ecbf
    
    Author: maxwen <max.weninger@gmail.com>
    Date:   Sat Nov 11 16:03:03 2017 +0100
    base: SystemUI: check for installed OmniJaws in qs details
    
    Change-Id: I1569eb2ce40bdc9eb9047447394d51ef91180bdf
    
    Change-Id: I78d5a0809280f7debfc3b843137c3902e050de1b

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5021,6 +5021,24 @@ public final class Settings {
          */
         public static final String LOCKSCREEN_MEDIA_METADATA = "lockscreen_media_metadata";
 
+
+        /**
+        * @hide
+         */
+        public static final String OMNIJAWS_WEATHER_ICON_PACK = "omnijaws_weather_icon_pack";
+
+        /**
+         * Status bar weather temperature
+         * 0: Hide the temperature
+         * 1: Display the temperature with scale and image
+         * 2: Display the temperature without scale and with image
+         * 3: Display the temparature with scale and without image
+         * 4: Display the temperature without scale and without image
+         * 5: Display the temperature image
+         * @hide
+         */
+        public static final String STATUS_BAR_SHOW_WEATHER_TEMP = "status_bar_show_weather_temp";
+
         /**
          * Three Finger Gesture from Oppo
          * @hide
