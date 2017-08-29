bbbea3463a6492868d266e718483abe8c426d9b5
maxwen <max.weninger@gmail.com>
2017-08-29 18:25:35 +0200

    Double tap to sleep on statusbar [1/2]
    abc ezio84: use existing lockscreen doubletap gesture listener
    and abcutils screenoff action
    
    Change-Id: Ib2f884047dd3754e96152148ea9bd042374a2045

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4472,6 +4472,12 @@ public final class Settings {
          */
         public static final String DOUBLE_TAP_SLEEP_LOCKSCREEN = "double_tap_sleep_lockscreen";
 
+	/**
+         *  Enable statusbar double tap gesture on to put device to sleep
+         * @hide
+         */
+        public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
+
         /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
@@ -4670,6 +4676,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(SHOW_BATTERY_PERCENT);
             PRIVATE_SETTINGS.add(DISPLAY_COLOR_MODE);
 	    PRIVATE_SETTINGS.add(DOUBLE_TAP_SLEEP_LOCKSCREEN);
+	    PRIVATE_SETTINGS.add(DOUBLE_TAP_SLEEP_GESTURE);
         }
 
         /**
