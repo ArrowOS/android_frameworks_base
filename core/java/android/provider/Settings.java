24242a4968cd134a1bd91a61c957494c66b40607
maxwen <max.weninger@gmail.com>
2017-09-21 00:38:01 +0200

    base: SystemUI: add back QS hide label
    if enabled label click (secondary) will be reassigned to long click
    for dual target tiles
    
    Change-Id: I440362ab38057cbfba7750d75825179bd830c4e8

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4504,6 +4504,16 @@ public final class Settings {
                 ANY_INTEGER_VALIDATOR;
 
         /**
+         * Whether to display qs tile titles in the qs panel
+         * @hide
+         */
+        public static final String QS_TILE_TITLE_VISIBILITY = "qs_tile_title_visibility";
+
+        /** @hide */
+        private static final Validator QS_TILE_TITLE_VISIBILITY_VALIDATOR =
+                BOOLEAN_VALIDATOR;
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -4570,6 +4580,7 @@ public final class Settings {
             NOTIFICATION_LIGHT_PULSE,
             QS_LAYOUT_COLUMNS_LANDSCAPE,
             QS_LAYOUT_COLUMNS,
+            QS_TILE_TITLE_VISIBILITY,
         };
 
         /**
@@ -4692,6 +4703,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(COLOR_BUCKET_OVERLAY);
             PRIVATE_SETTINGS.add(QS_LAYOUT_COLUMNS_LANDSCAPE);
             PRIVATE_SETTINGS.add(QS_LAYOUT_COLUMNS);
+            PRIVATE_SETTINGS.add(QS_TILE_TITLE_VISIBILITY);
         }
 
         /**
@@ -4788,6 +4800,7 @@ public final class Settings {
             VALIDATORS.put(QS_LAYOUT_COLUMNS_LANDSCAPE,
                     QS_LAYOUT_COLUMNS_LANDSCAPE_VALIDATOR);
             VALIDATORS.put(QS_LAYOUT_COLUMNS, QS_LAYOUT_COLUMNS_VALIDATOR);
+            VALIDATORS.put(QS_TILE_TITLE_VISIBILITY, QS_TILE_TITLE_VISIBILITY_VALIDATOR);
         }
 
         /**
