648df2a074b87bda906fd822e98412a8ff839215
Altaf-Mahdi <altaf.mahdi@gmail.com>
2017-11-02 19:05:43 -0400

    Replace recents clear all button with a fab button
    * this allows us to have it any corner
    * added top center and bottom right/left/center options
    
    Change-Id: If9c0bfd0ae9b2318cfd3ebd8a76e6b53d9694a31

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4514,16 +4514,16 @@ public final class Settings {
         public static final String POCKET_SERVICE = "pocket"; 
 
        /**
-         * location of the clear all rectents button
+         * show clear all recents button
          * @hide
          */
-        public static final String RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
+        public static final String SHOW_CLEAR_ALL_RECENTS = "show_clear_all_recents";
 
         /**
-         * show clear all recents button
-         *  @hide
+         * location of the clear all recents button
+         * @hide
          */
-        public static final String SHOW_CLEAR_ALL_RECENTS = "show_clear_all_recents";
+        public static final String  RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
