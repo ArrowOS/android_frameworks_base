78588aaa400ee7296615f10d709e2b263d7c74e5
Altaf-Mahdi <altaf.mahdi@gmail.com>
2017-11-15 22:36:51 -0500

    Add Screenshot QS Tile
    Change-Id: I9961ed353a9539a582a5fa3fdc4ffbaa10bab031

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -44,6 +44,7 @@ import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.SyncTile;
 import com.android.systemui.qs.tiles.UsbTetherTile;
 import com.android.systemui.qs.tiles.SoundTile;
+import com.android.systemui.qs.tiles.ScreenshotTile;
 import com.android.systemui.qs.tiles.UserTile;
 import com.android.systemui.qs.tiles.WeatherTile;
 import com.android.systemui.qs.tiles.WifiTile;
@@ -115,6 +116,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new WeatherTile(mHost);
             case "hwkeys":
                 return new HWKeysTile(mHost);
+	    case "screenshot":
+                return new ScreenshotTile(mHost);
         }
 
         // Intent tiles.
