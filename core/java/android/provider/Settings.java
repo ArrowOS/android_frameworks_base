f648968989d37493a14e2165ced6f9a07b5a20e5
maxwen <max.weninger@gmail.com>
2017-12-15 19:55:46 +0100

    Configure SystemUI theme [1/2]
    In 8.1 several elements change depending on the dominant color of the homescreen
    wallpaper. If is a light/white wallpaper these elements will turn white and if is
    a dark/black wallpaper then these elements will turn black. If is a neutral color
    then it defaults to the light/white color.
    
    This commits makes all of this configurable and so places the option in the user's
    hands. They can either leave it up to automatic selection (wallpaper api) or force
    a light or dark theme.
    
    -- Commit message was rewritten to better explain what's up :-)
    
    Original commit message:
    
    allow to force light or dark or use the wallpaper based
    automatic selection
    
    Change-Id: Id7c287ea82cf76e6d3189c5621258cefe2c4d6d2

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4262,6 +4262,14 @@ public final class Settings {
         public static final String SWAP_VOLUME_BUTTONS = "swap_volume_buttons";
 
         /**
+         * 0 - wallpaper based
+         * 1 - force light
+         * 2 - force dark
+         * @hide
+         */
+        public static final String SYSTEM_UI_THEME = "system_ui_theme";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
