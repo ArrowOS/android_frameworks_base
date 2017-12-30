c5dd2c314de5b13ad7b0c713de52ea19361493a7
Alex Cruz <du.alexcruz@gmail.com>
2017-12-30 03:46:18 -0500

    Make the power menu configurable [1/2]
    This allows to toggle all the options that we've added and some AOSP ones
    
    - Reboot
    - Advanced reboot (depends on reboot menu)
    - Airplane mode
    - Screenshot
    
    Went ahead and commented out the other AOSP options in case a user wants to
    get cute and toggle these on via a overlay. Also anymore than 5 options and
    things start to look janky.
    
    Change-Id: I5d86cef181a8f12ad18e16ccc1e8c75879b3e569

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3936,6 +3936,34 @@ public final class Settings {
          */
 
         /**
+         * Whether to display the reboot option in the power menu
+         *
+         * @hide
+         */
+        public static final String POWERMENU_REBOOT = "powermenu_reboot";
+
+        /**
+         * Whether to display the advanced reboot option in the power menu
+         *
+         * @hide
+         */
+        public static final String POWERMENU_ADVANCED_REBOOT = "powermenu_advanced_reboot";
+
+        /**
+         * Whether to display the screenshot option in the power menu
+         *
+         * @hide
+         */
+        public static final String POWERMENU_SCREENSHOT = "powermenu_screenshot";
+
+        /**
+         * Whether to display the airplane option in the power menu
+         *
+         * @hide
+         */
+        public static final String POWERMENU_AIRPLANE = "powermenu_airplane";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
