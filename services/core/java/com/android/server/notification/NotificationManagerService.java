45e6c2233d1f792ade7c5f768fab323602faeafb
ezio84 <brabus84@gmail.com>
2018-02-01 16:49:32 +0100

    Add api to force show led lights
    We can use this to show a led color preview when using the
    color picker, or any other stuff.
    
    use -1 as color int to switch off
    
    Change-Id: Id59f480e74a30a875c6640fdfc2e746d98a8513c
    
    Led light preview: switch led off before changing the color
    
    on some devices this can avoid glitches
    
    Change-Id: I4ef51d6b074e344d48f4a814549db2dad9ef9d0a

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -4143,6 +4143,16 @@ public class NotificationManagerService extends SystemService {
             new NotificationShellCmd(NotificationManagerService.this)
                     .exec(this, in, out, err, args, callback, resultReceiver);
         }
+
+        @Override
+        public void forceShowLedLight(int color) {
+            forceShowLed(color);
+        }
+
+        @Override
+        public void forcePulseLedLight(int color, int onTime, int offTime) {
+            forcePulseLed(color, onTime, offTime);
+        }
     };
 
     @VisibleForTesting
@@ -5778,6 +5788,24 @@ public class NotificationManagerService extends SystemService {
             EventLogTags.writeNotificationAlert(key, buzz ? 1 : 0, beep ? 1 : 0, blink ? 1 : 0);
         }
         record.setAudiblyAlerted(buzz || beep);
+    }
+
+    private void forceShowLed(int color) {
+        if (color != -1) {
+            mNotificationLight.turnOff();
+            mNotificationLight.setColor(color);
+        } else {
+            mNotificationLight.turnOff();
+        }
+    }
+
+    private void forcePulseLed(int color, int onTime, int offTime) {
+        if (color != -1) {
+            mNotificationLight.turnOff();
+            mNotificationLight.setFlashing(color, Light.LIGHT_FLASH_TIMED, onTime, offTime);
+        } else {
+            mNotificationLight.turnOff();
+        }
     }
 
     @GuardedBy("mNotificationLock")
