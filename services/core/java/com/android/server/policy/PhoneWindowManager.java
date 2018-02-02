023906905b75a1e1ad657e3959ec1f7e3665a913
Alex Cruz <du.alexcruz@gmail.com>
2018-02-02 05:21:19 -0500

    Disable power button haptic feedback conditionally
    When the power menu is hidden on the lockscreen there's no need for the power
    button to use haptic feedback. It does nothing, poor UX. Only use HF when the
    power menu is visible.
    
    Change-Id: I97866c03398e2ab442f90dc9ac5db53b2d98d2e5

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -1544,7 +1544,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             break;
         case LONG_PRESS_POWER_GLOBAL_ACTIONS:
             mPowerKeyHandled = true;
-            performHapticFeedbackLw(null, HapticFeedbackConstants.LONG_PRESS, false);
             KeyguardManager km = (KeyguardManager) mContext.getSystemService(Context.KEYGUARD_SERVICE);
             boolean locked = km.inKeyguardRestrictedInputMode();
             boolean globalActionsOnLockScreen = Settings.System.getInt(
@@ -1552,6 +1551,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             if (locked && !globalActionsOnLockScreen) {
                 behavior = LONG_PRESS_POWER_NOTHING;
             } else {
+                performHapticFeedbackLw(null, HapticFeedbackConstants.LONG_PRESS, false);
                 showGlobalActionsInternal();
             }
             break;
