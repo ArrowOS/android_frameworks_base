4ddec9157d173e083f0d92eac644f02425dd170b
Marko Man <darkobas@gmail.com>
2018-02-14 09:43:11 +0100

    base: add Navbar tile
    Change-Id: Ia79f7feb015a3f2e00272fd6a1f5fc24abd100fb

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -38,6 +38,7 @@ import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.HWKeysTile;
 import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
+import com.android.systemui.qs.tiles.NavbarDisableTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.RebootTile;
@@ -125,6 +126,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new ScreenshotTile(mHost);
             case "reboot":
                 return new RebootTile(mHost);
+            case "navbardisable":
+                return new NavbarDisableTile(mHost);
         }
 
         // Intent tiles.
