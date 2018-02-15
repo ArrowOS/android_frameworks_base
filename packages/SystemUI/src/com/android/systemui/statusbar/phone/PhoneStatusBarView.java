ee6d831acf622dd3b903b05c886c2a55e88f0171
Roman Birg <romanbirg@gmail.com>
2018-02-15 10:22:23 -0800

    SystemUI: properly adjust statusbar height on rotate as necessary
    Some devices may want a different statusbar height between landscape and
    portrait.
    
    For this to work, we need to read the statusbar height from the internal
    framework where the statusbar height overlay happens.
    
    Change-Id: I34b5549fee5e3fe7c4810dfecb66087e28b01d74
    Signed-off-by: Roman Birg <romanbirg@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
@@ -290,8 +290,8 @@ public class PhoneStatusBarView extends PanelBar {
                 R.dimen.display_cutout_margin_consumption);
 
         ViewGroup.LayoutParams layoutParams = getLayoutParams();
-        layoutParams.height = getResources().getDimensionPixelSize(
-                R.dimen.status_bar_height);
+        layoutParams.height = mContext.getResources().getDimensionPixelSize(
+                com.android.internal.R.dimen.status_bar_height);
         setLayoutParams(layoutParams);
     }
 
