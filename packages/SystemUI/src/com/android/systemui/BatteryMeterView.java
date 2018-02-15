c29d7bf0524bcb212015f7aed560c2f0625cc4c9
Anas Karbila <anaskarbila@aospa.co>
2018-02-15 12:13:46 +0100

    Battery:Text: don't use additional resources for no padding
    Change-Id: I5430bbe5313ba1b731f140c785cee78f3f7a01e8
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
@@ -84,7 +84,6 @@ public class BatteryMeterView extends LinearLayout implements
     private boolean mCharging;
 
     private final int mEndPadding;
-    private final int mEndPaddingNoIcon;
 
     private boolean mQsHeaderOrKeyguard;
 
@@ -135,8 +134,6 @@ public class BatteryMeterView extends LinearLayout implements
         addView(mBatteryIconView, mlp);
 
         mEndPadding = res.getDimensionPixelSize(R.dimen.battery_level_padding_start);
-        mEndPaddingNoIcon = res.getDimensionPixelSize(
-                R.dimen.battery_level_padding_start_no_icon);
         updateShowPercent();
         setColorsFromContext(context);
         // Init to not dark at all.
@@ -295,7 +292,7 @@ public class BatteryMeterView extends LinearLayout implements
         }
         if (mBatteryPercentView != null) {
             mBatteryPercentView.setPaddingRelative(0, 0,
-                    mStyle == BatteryMeterDrawableBase.BATTERY_STYLE_TEXT ? mEndPaddingNoIcon : mEndPadding, 0);
+                    mStyle == BatteryMeterDrawableBase.BATTERY_STYLE_TEXT ? 0 : mEndPadding, 0);
         }
         mDrawable.showPercentInsideCircle(!mShowPercentText);
     }
