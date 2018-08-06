bd479505b638800ac43a3109e02341e27bde0da2
Joe Maples <joe@frap129.org>
2018-08-06 18:42:21 -0400

    base: Tuner: Don't clear out prefs, it's not disabled
    Change-Id: I7bea54fa19c61f3c769c0bafd9ea359ad1bd3356
    Signed-off-by: Joe Maples <joe@frap129.org>

--- packages/SystemUI/src/com/android/systemui/tuner/TunerServiceImpl.java
+++ packages/SystemUI/src/com/android/systemui/tuner/TunerServiceImpl.java
@@ -15,12 +15,15 @@
  */
 package com.android.systemui.tuner;
 
+import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.pm.UserInfo;
 import android.database.ContentObserver;
 import android.net.Uri;
@@ -41,6 +44,9 @@ import com.android.systemui.dagger.SysUISingleton;
 import com.android.systemui.dagger.qualifiers.Main;
 import com.android.systemui.demomode.DemoModeController;
 import com.android.systemui.qs.QSTileHost;
+import com.android.systemui.R;
+import com.android.systemui.SystemUI;
+import com.android.systemui.SystemUIApplication;
 import com.android.systemui.settings.UserTracker;
 import com.android.systemui.statusbar.phone.StatusBarIconController;
 import com.android.systemui.statusbar.phone.SystemUIDialog;
@@ -61,8 +67,6 @@ public class TunerServiceImpl extends TunerService {
     private static final String TAG = "TunerService";
     private static final String TUNER_VERSION = "sysui_tuner_version";
 
-    private static final int CURRENT_TUNER_VERSION = 4;
-
     // Things that use the tunable infrastructure but are now real user settings and
     // shouldn't be reset with tuner settings.
     private static final String[] RESET_EXCEPTION_LIST = new String[] {
@@ -71,6 +75,8 @@ public class TunerServiceImpl extends TunerService {
             Settings.Secure.MEDIA_CONTROLS_RESUME,
             Settings.Secure.MEDIA_CONTROLS_RECOMMENDATION
     };
+
+    private static final int CURRENT_TUNER_VERSION = 5;
 
     private final Observer mObserver = new Observer();
     // Map of Uris we listen on to their settings keys.
@@ -150,12 +156,7 @@ public class TunerServiceImpl extends TunerService {
         }
         // 2 Removed because we want tuner.
         // 3 Removed because of a revert.
-        if (oldVersion < 4) {
-            // Delay this so that we can wait for everything to be registered first.
-            final int user = mCurrentUser;
-            mainHandler.postDelayed(
-                    () -> clearAllFromUser(user), 5000);
-        }
+        // 4 Removed since we aren't filtering prefs.
         setValue(TUNER_VERSION, newVersion);
     }
 
@@ -247,6 +248,9 @@ public class TunerServiceImpl extends TunerService {
 
     private void reloadAll() {
         for (String key : mTunableLookup.keySet()) {
+            if (ArrayUtils.contains(RESET_EXCEPTION_LIST, key)) {
+                continue;
+            }
             String value = Settings.Secure.getStringForUser(mContentResolver, key,
                     mCurrentUser);
             for (Tunable tunable : mTunableLookup.get(key)) {
@@ -267,9 +271,6 @@ public class TunerServiceImpl extends TunerService {
 
         // A couple special cases.
         for (String key : mTunableLookup.keySet()) {
-            if (ArrayUtils.contains(RESET_EXCEPTION_LIST, key)) {
-                continue;
-            }
             Settings.Secure.putStringForUser(mContentResolver, key, null, user);
         }
     }
