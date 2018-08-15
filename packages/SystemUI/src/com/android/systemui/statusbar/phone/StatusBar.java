05340e34bd9c1175bccd8c6609c44b4b3173fe20
maxwen <max.weninger@gmail.com>
2018-08-15 20:28:27 +0200

    fwb: Implement alternative device key handler
    @jhenrique09 edits:
    * Adapt to PE
    * Minify code
    
    This is omni's rom keyhandler implementation
    
    You can't use it if config_deviceKeyHandlerLibs/config_deviceKeyHandlerClasses (LOS implementation) is defined
    
    Change-Id: I5b37c4cf251da1de32498ab707914b5c2c752b7a

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -1156,6 +1156,7 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
         filter.addAction(Intent.ACTION_SCREEN_ON);
         filter.addAction(DevicePolicyManager.ACTION_SHOW_DEVICE_MONITORING_DIALOG);
         filter.addAction("android.intent.action.SCREEN_CAMERA_GESTURE");
+        filter.addAction("com.android.systemui.ACTION_DISMISS_KEYGUARD");
         context.registerReceiverAsUser(mBroadcastReceiver, UserHandle.ALL, filter, null, null);
 
         IntentFilter demoFilter = new IntentFilter();
@@ -3283,6 +3284,11 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
                 }
 
                 onCameraLaunchGestureDetected(StatusBarManager.CAMERA_LAUNCH_SOURCE_SCREEN_GESTURE);
+            }else if ("com.android.systemui.ACTION_DISMISS_KEYGUARD".equals(action)) {
+                if (intent.hasExtra("launch")) {
+                    Intent launchIntent = (Intent) intent.getParcelableExtra("launch");
+                    startActivityDismissingKeyguard(launchIntent, true, true);
+                }
             }
         }
     };
