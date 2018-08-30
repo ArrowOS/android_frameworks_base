3b755a14452d8950f5f99a36167a0b2c7e6fb515
maxwen <max.weninger@gmail.com>
2018-08-30 12:25:45 +0200

    base: add back weather parts [1/3]
    * neobuddy89: Updated for Android 10 and Android 11.
    
    Includes below changes:
    
        From: LorDClockaN <lordclockan@gmail.com>
        Date: Mon, 6 Mar 2017 22:32:58 +0100
        Subject: [PATCH 5/9] WeatherTile: Start Google Weather on LongClick
    
        Change-Id: I049e026bcdcdea34ed61a07e6de953d91c6e57c1
    
        From: SpiritCroc <spiritcroc@gmail.com>
        Date: Wed, 8 Mar 2017 20:26:35 +0100
        Subject: [PATCH 6/9] Weather tile longclick: alternative weather apps when
         google weather not installed
    
        Change-Id: I2ea721a53d63dbd80edcc7a29e791f89c8cc0859
    
        From: El Dainosor <eldainosor@gmail.com>
        Date: Thu, 22 Mar 2018 00:11:04 +0000
        Subject: [PATCH 7/9] Weather tile: Use the alternate apps first
    
        This will make the weather tile, on long press, start the alternative weather apps instead of shoveling google weather no matter what
        Thanks ishubhamsingh for the help with the code part
    
        Change-Id: I92839d79ec6e472fc619fee2dfb9fd4d27605708
    
        From: El Dainosor <eldainosor@gmail.com>
        Date: Sat, 18 Jan 2020 21:58:09 +0100
        Subject: [PATCH 9/9] WeatherTile: Send weather info to a Secondary Label
    
        Instead of making the actual status the title, let's show it as
        secondary text, else it's a bad, ugly, meanie QS tile.
    
        Change-Id: Ifc4c5f12e6759d016ce77a35c6bdf0d8df2db8ce
    
    Change-Id: I1468acaa5a3b5d74d6627d33ac29f43850a4d146

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5022,6 +5022,11 @@ public final class Settings {
         public static final String LOCKSCREEN_MEDIA_METADATA = "lockscreen_media_metadata";
 
         /**
+         * @hide
+         */
+        public static final String OMNIJAWS_WEATHER_ICON_PACK = "omnijaws_weather_icon_pack";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
