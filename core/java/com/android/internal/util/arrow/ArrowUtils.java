ac42a0f0107ec42da0190c6593212d64c4b806f0
romanbb <romanbirg@gmail.com>
2018-08-30 11:04:47 +0200

    ArrowUtils: add isPackageInstalled boolean
    Change-Id: Ib23bdb4cf2f8f0ed909f52ac2dab6e18fe9a91d4

--- core/java/com/android/internal/util/arrow/ArrowUtils.java
+++ core/java/com/android/internal/util/arrow/ArrowUtils.java
@@ -30,6 +30,9 @@ import android.view.KeyEvent;
 import android.os.Handler;
 import android.os.UserHandle;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
+
 import com.android.internal.statusbar.IStatusBarService;
 
 /**
@@ -42,6 +45,25 @@ public class ArrowUtils {
         if (pm!= null) {
             pm.goToSleep(SystemClock.uptimeMillis());
         }
+    }
+
+    public static boolean isPackageInstalled(Context context, String pkg, boolean ignoreState) {
+        if (pkg != null) {
+            try {
+                PackageInfo pi = context.getPackageManager().getPackageInfo(pkg, 0);
+                if (!pi.applicationInfo.enabled && !ignoreState) {
+                    return false;
+                }
+            } catch (NameNotFoundException e) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean isPackageInstalled(Context context, String pkg) {
+        return isPackageInstalled(context, pkg, true);
     }
 
     public static boolean deviceHasFlashlight(Context ctx) {
