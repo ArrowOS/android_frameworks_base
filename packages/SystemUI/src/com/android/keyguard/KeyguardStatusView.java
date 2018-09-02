d86fbec0276dc7474347d323ae933cd1fbbcac2f
maxwen <max.weninger@gmail.com>
2018-09-02 20:28:56 +0200

    base: SystemUI: hide keyguard weather when pulsing
    Change-Id: Ied8d34fbdfcfbd0deda47a783cacebaaa58bf1e4

--- packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
@@ -81,6 +81,7 @@ public class KeyguardStatusView extends GridLayout implements
     private int mIconTopMarginWithHeader;
     private boolean mShowingHeader;
     private CurrentWeatherView mWeatherView;
+    private boolean mShowWeather;
 
     private KeyguardUpdateMonitorCallback mInfoCallback = new KeyguardUpdateMonitorCallback() {
 
@@ -451,6 +452,9 @@ public class KeyguardStatusView extends GridLayout implements
             return;
         }
         mPulsing = pulsing;
+        if (mWeatherView != null) {
+            mWeatherView.setVisibility((mShowWeather && !mPulsing) ? View.VISIBLE : View.GONE);
+        }
     }
 
     private boolean shouldShowLogout() {
@@ -471,16 +475,16 @@ public class KeyguardStatusView extends GridLayout implements
     private void updateSettings() {
         final ContentResolver resolver = getContext().getContentResolver();
         final Resources res = getContext().getResources();
-        boolean showWeather = Settings.System.getIntForUser(resolver,
+        mShowWeather = Settings.System.getIntForUser(resolver,
                 Settings.System.OMNI_LOCKSCREEN_WEATHER_ENABLED, 0,
                 UserHandle.USER_CURRENT) == 1;
 
         if (mWeatherView != null) {
-            if (showWeather) {
+            if (mShowWeather) {
                 mWeatherView.setVisibility(View.VISIBLE);
                 mWeatherView.enableUpdates();
             }
-            if (!showWeather) {
+            if (!mShowWeather) {
                 mWeatherView.setVisibility(View.GONE);
                 mWeatherView.disableUpdates();
             }
