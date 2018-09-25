51e492a7887cad72c101297b40d3878317058d07
xyyx <xyyx@mail.ru>
2018-09-25 19:57:45 +0000

    base: Unlock keystore with fingerprint after reboot (1/2)
    Change-Id: I9b565d258ec060e4d3b7bb4db0d0790ad5e9c037

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4592,6 +4592,12 @@ public final class Settings {
                 "torch_power_button_gesture";
 
         /**
+         * Unlock keystore with fingerprint after reboot
+         * @hide
+         */
+        public static final String FP_UNLOCK_KEYSTORE = "fp_unlock_keystore";
+
+       /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
