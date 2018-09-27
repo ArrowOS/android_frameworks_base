bbaedaac37bb017b2238f3a16468801bc8d541de
Chiachang Wang <chiachangwang@google.com>
2018-09-27 21:55:32 +0800

    ConnectivityService: Fix idle timer rule leakage
    Idle timer rule is not cleared as expected if there is a
    default network replacement.
    
    Bug:37080406
    Test: 1.run frameworks-net
          2.check iptables rule with default network replacement
    
    Change-Id: I6bd29d79e4ca3e8de4b867c4fcb5f81d02ba6de4

--- services/core/java/com/android/server/ConnectivityService.java
+++ services/core/java/com/android/server/ConnectivityService.java
@@ -1887,6 +1887,19 @@ public class ConnectivityService extends IConnectivityManager.Stub
     }
 
     /**
+     * Update data activity tracking when network state is updated.
+     */
+    private void updateDataActivityTracking(NetworkAgentInfo newNetwork,
+            NetworkAgentInfo oldNetwork) {
+        if (newNetwork != null) {
+            setupDataActivityTracking(newNetwork);
+        }
+        if (oldNetwork != null) {
+            removeDataActivityTracking(oldNetwork);
+        }
+    }
+
+    /**
      * Reads the network specific MTU size from reources.
      * and set it on it's iface.
      */
@@ -2512,7 +2525,7 @@ public class ConnectivityService extends IConnectivityManager.Stub
         }
         nai.clearLingerState();
         if (nai.isSatisfyingRequest(mDefaultRequest.requestId)) {
-            removeDataActivityTracking(nai);
+            updateDataActivityTracking(null /*new Network*/, nai);
             notifyLockdownVpn(nai);
             ensureNetworkTransitionWakelock(nai.name());
         }
@@ -5169,7 +5182,6 @@ public class ConnectivityService extends IConnectivityManager.Stub
 
     private void makeDefault(NetworkAgentInfo newNetwork) {
         if (DBG) log("Switching to new default network: " + newNetwork);
-        setupDataActivityTracking(newNetwork);
         try {
             mNetd.setDefaultNetId(newNetwork.network.netId);
         } catch (Exception e) {
@@ -5344,6 +5356,7 @@ public class ConnectivityService extends IConnectivityManager.Stub
             }
         }
         if (isNewDefault) {
+            updateDataActivityTracking(newNetwork, oldDefaultNetwork);
             // Notify system services that this network is up.
             makeDefault(newNetwork);
             // Log 0 -> X and Y -> X default network transitions, where X is the new default.
