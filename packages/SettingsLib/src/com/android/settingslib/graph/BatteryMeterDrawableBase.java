c4bfcce530715ed9c7c5f808c0d535bc8087f582
camcory <camcory1@gmail.com>
2018-10-11 02:55:10 +0200

    Add the powersave hint to the circle battery
    Arrow edits:- increased the size of orange circle
    looks better now
    
    Change-Id: Ifa90cf446bfe8059d3ae50aa8c3b9e52ec2dee45

--- packages/SettingsLib/src/com/android/settingslib/graph/BatteryMeterDrawableBase.java
+++ packages/SettingsLib/src/com/android/settingslib/graph/BatteryMeterDrawableBase.java
@@ -601,6 +601,16 @@ public class BatteryMeterDrawableBase extends Drawable {
             c.drawArc(mFrame, 270, 3.6f * level, false, mBatteryPaint);
         }
 
+        if (!mCharging && mPowerSaveEnabled && mPowerSaveAsColorError) {
+        mFrame.set(
+                strokeWidth / 3f + mPadding.left,
+                strokeWidth / 3f,
+                circleSize - strokeWidth / 3f + mPadding.left,
+                circleSize - strokeWidth / 3f);
+
+           c.drawArc(mFrame, 270, 3.6f * level, false, mPowersavePaint);
+        }
+
         final int height = mHeight;
         final int width = (int) (getAspectRatio() * mHeight);
         // compute percentage text
