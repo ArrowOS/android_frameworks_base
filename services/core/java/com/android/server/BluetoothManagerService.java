a887019349fd1d87c6a745597381b50bd047714d
M Safoorah Banu <safoorah@codeaurora.org>
2018-10-16 10:41:25 +0530

    BT: API to update quiet mode status in Adapter service [2/3]
     - In BLE on use case, quiet mode status is not updated to
       Adapter service which results in not initiating auto
       connection of hfp & a2dp though quiet mode is disabled
       for next turn On.
    
     - Added new API to update quiet mode status in Adapter
       service.
    
    CRs-Fixed: 2337538
    Change-Id: I49a604b7996024fd9fd498fb7cc6aafc208517a5

--- services/core/java/com/android/server/BluetoothManagerService.java
+++ services/core/java/com/android/server/BluetoothManagerService.java
@@ -1080,6 +1080,7 @@ class BluetoothManagerService extends IBluetoothManager.Stub {
             }
             if (isBluetoothPersistedStateOnBluetooth() || !isBleAppPresent()) {
                 // This triggers transition to STATE_ON
+                mBluetooth.updateQuietModeStatus(mQuietEnable);
                 mBluetooth.onLeServiceUp(mContext.getAttributionSource());
                 persistBluetoothSetting(BLUETOOTH_ON_BLUETOOTH);
             }
@@ -1889,6 +1890,8 @@ class BluetoothManagerService extends IBluetoothManager.Stub {
                     mHandler.removeMessages(MESSAGE_RESTART_BLUETOOTH_SERVICE);
                     mEnable = true;
 
+                    mQuietEnable = (msg.arg1 == 1);
+
                     if (isBle == 0) {
                         persistBluetoothSetting(BLUETOOTH_ON_BLUETOOTH);
                     }
@@ -1905,6 +1908,7 @@ class BluetoothManagerService extends IBluetoothManager.Stub {
                                         Slog.i(TAG, "Already at BLE_ON State");
                                     } else {
                                         Slog.w(TAG, "BT Enable in BLE_ON State, going to ON");
+                                        mBluetooth.updateQuietModeStatus(mQuietEnable);
                                         mBluetooth.onLeServiceUp(mContext.getAttributionSource());
                                     }
                                     break;
