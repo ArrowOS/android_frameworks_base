3fa6002cac449f491090d7a99c318af05691f89b
SagarMakhar <sagarmakhar@gmail.com>
2018-10-22 18:50:29 +0000

    base: Add VOLTE icon toggle [1/2]
     * Disable by default
    
    Change-Id: I28a981517213509c0a40a815c6e40c695fea6c68

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5374,6 +5374,12 @@ public final class Settings {
         public static final String STATUS_BAR_BATTERY_TEXT_CHARGING = "status_bar_battery_text_charging";
 
         /**
+         * Whether to show VoLTE icon or not
+         * @hide
+         */
+        public static final String SHOW_VOLTE_ICON = "show_volte_icon";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
