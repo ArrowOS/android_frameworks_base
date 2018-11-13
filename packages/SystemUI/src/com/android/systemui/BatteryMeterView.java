1364495011e06274f8a222940ed288ef4d744d45
bigrushdog <randall.rushing@gmail.com>
2018-11-13 05:30:20 +0000

    base: Update keyguard when font changes
    removed unused variables
    
    Change-Id: If9305cbdeff2dad8e7a4bbcd073d94e20d9720b0

--- packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
@@ -28,6 +28,7 @@ import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.database.ContentObserver;
 import android.graphics.Rect;
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Handler;
 import android.os.UserHandle;
@@ -66,7 +67,7 @@ public class BatteryMeterView extends LinearLayout implements
     private ImageView mBatteryIconView;
     private final CurrentUserTracker mUserTracker;
     private TextView mBatteryPercentView;
-
+    private static final String FONT_FAMILY = "sans-serif-medium";
     private BatteryController mBatteryController;
     private SettingObserver mSettingObserver;
 
@@ -277,11 +278,13 @@ public class BatteryMeterView extends LinearLayout implements
     }
 
     private void updatePercentText() {
+        Typeface tf = Typeface.create(FONT_FAMILY, Typeface.NORMAL);
         if (mBatteryPercentView != null) {
             CharSequence mChargeIndicator =
                     mCharging && mStyle == BatteryMeterDrawableBase.BATTERY_STYLE_TEXT ? "~" : "";
             mBatteryPercentView.setText(mChargeIndicator +
                     NumberFormat.getPercentInstance().format(mLevel / 100f));
+            mBatteryPercentView.setTypeface(tf);
         }
     }
 
