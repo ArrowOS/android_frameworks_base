5aa862935a728b7a383a8ba990c359e296b2f4d1
Alex Cruz <du.alexcruz@gmail.com>
2018-12-30 00:24:00 -0600

    Hide lockscreen items [1/2]
    This adds the ability to add items such as data, clock, zen, weather
    and alarm on the lockscreen.
    
    Thanks to ezio84 for part of his code to update the keyguard
    https://github.com/DirtyUnicorns/android_frameworks_base/commit/67f9b611c4cdff22dff5d7cb03ef49afa8f1f09b
    
    Change-Id: Ic3b5cac2d0ff16d6c4b1949bf447bd1621c1388f

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -2849,6 +2849,10 @@ public class NotificationPanelView extends PanelView implements
         }
     }
 
+    public void updateKeyguardStatusSettings() {
+        mKeyguardStatusView.updateAll();
+    }
+
     public void setStatusAccessibilityImportance(int mode) {
          mKeyguardStatusView.setImportantForAccessibility(mode);
     }
