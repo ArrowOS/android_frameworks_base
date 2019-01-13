4a25dda128285b1f2060ce511af8d51fa7aa42a7
Alex Cruz <du.alexcruz@gmail.com>
2019-01-13 10:59:49 -0600

    Bring back traffic indicator for all!
    arrow-edits:
     - adapt to Q
    
    AEX Edits
     - Adapt to Android 12
    
    Change-Id: Ia41474a262b258f8a2d4d7b10a15891729ffd17c

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5206,6 +5206,18 @@ public final class Settings {
          */
         public static final String VOLUME_BUTTON_MUSIC_CONTROL = "volume_button_music_control";
 
+        /**
+         * Wheter to show network traffic indicator in statusbar
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_STATE = "network_traffic_state";
+
+        /**
+         * Network traffic inactivity threshold (default is 1 kBs)
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD = "network_traffic_autohide_threshold";
+
        /**
          * These are all public system settings
          *
