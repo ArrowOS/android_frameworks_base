ac4450586b4bbcd6609b2f26d718e8816a5ba1b6
ganeshi4u <ganeshvarmai4u@gmail.com>
2019-02-08 06:19:22 +0100

    Make weather clickable in QS status bar header [1/2]
    * Requires weather activity declaration inside settings manifest
    
    Signed-off-by: ganeshi4u <ganeshvarmai4u@gmail.com>
    Change-Id: If0becae97b7dbb78f59a6e252347668ad6ca5cf9

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -27,6 +27,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.content.ComponentName;
 import android.graphics.Color;
 import android.graphics.Rect;
 import android.media.AudioManager;
@@ -126,6 +127,10 @@ public class QuickStatusBarHeader extends RelativeLayout implements
     /** Counts how many times the long press tooltip has been shown to the user. */
     private int mShownCount;
 
+    // Statusbar Weather Image
+    private View mWeatherImageView;
+    private View mWeatherTextView;
+
     private final BroadcastReceiver mRingerReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
@@ -187,6 +192,10 @@ public class QuickStatusBarHeader extends RelativeLayout implements
         mBatteryMeterView.setOnClickListener(this);
         mClockView = findViewById(R.id.clock);
         mClockView.setOnClickListener(this);
+	mWeatherTextView = findViewById(R.id.weather_temp);
+	mWeatherTextView.setOnClickListener(this);
+        mWeatherImageView = findViewById(R.id.weather_image);
+	mWeatherImageView.setOnClickListener(this);
         mDateView = findViewById(R.id.date);
         mDateView.setOnClickListener(this);
     }
@@ -454,7 +463,13 @@ public class QuickStatusBarHeader extends RelativeLayout implements
             builder.appendPath(Long.toString(System.currentTimeMillis()));
             Intent todayIntent = new Intent(Intent.ACTION_VIEW, builder.build());
             Dependency.get(ActivityStarter.class).postStartActivityDismissingKeyguard(todayIntent, 0);
-        }
+        } else if (v == mWeatherImageView || v == mWeatherTextView) {
+	    Intent weatherIntent = new Intent();
+	    weatherIntent.setClassName("com.android.settings", "com.android.settings.Settings$ArrowWeather");
+	    weatherIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK |
+                        Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+            Dependency.get(ActivityStarter.class).postStartActivityDismissingKeyguard(weatherIntent, 0);
+	}
     }
 
     @Override
