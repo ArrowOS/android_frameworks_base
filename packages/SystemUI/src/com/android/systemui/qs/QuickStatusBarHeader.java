9a5d0c60e9d29a4ce62245c388911276e0d0508b
ganeshi4u <ganeshvarmai4u@gmail.com>
2019-02-08 06:53:39 +0100

    Make traffic clickable in QS status bar header [1/2]
    * Requires Traffic activity declaration inside settings manifest
    
    Signed-off-by: ganeshi4u <ganeshvarmai4u@gmail.com>
    Change-Id: I6137c9bf3e144f6591d738452c9c3e353f753f10

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -131,6 +131,9 @@ public class QuickStatusBarHeader extends RelativeLayout implements
     private View mWeatherImageView;
     private View mWeatherTextView;
 
+    // Statusbar Traffic
+    private View mTraffic;
+
     private final BroadcastReceiver mRingerReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
@@ -197,6 +200,8 @@ public class QuickStatusBarHeader extends RelativeLayout implements
 	mWeatherImageView.setOnClickListener(this);
         mDateView = findViewById(R.id.date);
         mDateView.setOnClickListener(this);
+	mTraffic = findViewById(R.id.networkTraffic);
+	mTraffic.setOnClickListener(this);
     }
 
     private void updateStatusText() {
@@ -466,6 +471,12 @@ public class QuickStatusBarHeader extends RelativeLayout implements
 	    Intent weatherIntent = new Intent();
 	    weatherIntent.setClassName("com.android.settings", "com.android.settings.Settings$ArrowWeather");
 	    weatherIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK |
+                        Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+            Dependency.get(ActivityStarter.class).postStartActivityDismissingKeyguard(weatherIntent, 0);
+	} else if (v == mTraffic) {
+	    Intent weatherIntent = new Intent();
+            weatherIntent.setClassName("com.android.settings", "com.android.settings.Settings$ArrowTraffic");
+            weatherIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK |
                         Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
             Dependency.get(ActivityStarter.class).postStartActivityDismissingKeyguard(weatherIntent, 0);
 	}
