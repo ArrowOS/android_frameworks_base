81ad05e5b7f2ee208166b49d8334525826c71e32
ganeshi4u <ganeshvarmai4u@gmail.com>
2019-03-12 16:01:38 +0100

    NetworkTrafficSB: OOS style network traffic indicator
    - add separate dimens. values for SBtraffic
    - adjust size and alignment
    - reduce left right padding
    - make traffic text bold/font
    - set gravity to right
    - nuked arrow indicators
    - upload and download speeds view switch depending on which is higher
    
    Change-Id: I48a16dc383ce333f0a64aa87b994d300b26e6a9c
    Signed-off-by: ganeshi4u <ganeshvarmai4u@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
@@ -315,7 +315,7 @@ public interface StatusBarIconController {
 
         private NetworkTrafficSB onCreateNetworkTraffic(String slot) {
             NetworkTrafficSB view = new NetworkTrafficSB(mContext);
-            view.setPadding(4, 0, 4, 0);
+            view.setPadding(3, 6, 3, 0);
             return view;
         }
 
