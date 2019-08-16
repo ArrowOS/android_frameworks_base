930c7543223da833b835e442969e83015fa346d0
Sam Mortimer <sam@mortimer.me.uk>
2019-08-15 19:40:55 -0700

    fw/b: Add capability to allow tethering to use VPN upstreams
    Squash of:
    
    Author: Sam Mortimer <sam@mortimer.me.uk>
    Date:   Thu Aug 15 19:40:55 2019 -0700
    
        fw/b: Add capability to allow tethering to use VPN upstreams
    
        * Toggled on/off at runtime via a new hotspot lineage setting.
    
        * Dynamically updates the tethering upstream for existing hotspot
          clients as VPNs are brought up / down or the hotspot setting
          is changed.
    
        * This implementation depends on fw/b config_tether_upstream_automatic
          being set to true.
    
        Change-Id: I2ac0b4acc0ea686dfdf54561cb3428808e337160
    
    Change-Id: I2363e9baa007eac25be04ce23f0de41b80c5da42
    
    ----
    
    Author: Sam Mortimer <sam@mortimer.me.uk>
    Date:   Fri Sep 13 16:27:37 2019 -0700
    
        fw/b: Prevent trying to enable hw offload for tethering via VPN upstreams
    
        * Tethering via VPN upstream requires a sw path.
    
        * hw offload setup happened to be being disabled anyway owing to a fail
          return code from setDataLimit().  However, it was causing offload to be
          disabled entirely (until next hotspot off / on event).
    
        * Gracefully skip hw offload for vpn upstreams so that it is automatically
          used again when a vpn is disconnected.
    
        Change-Id: I4df13f02889305560903b7b1e919eedc7af78c07
    
    [mkbestas]: Remove LineageSettings dependency & port to android 11
    
    [uldiniad]: Replaced Settings.Secure private APIs and variables with alternatives
    
    Change-Id: I77ed0142e653f4993486eea44e4dac21e3f67f17

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -9318,6 +9318,11 @@ public final class Settings {
         public static final String TAP_GESTURE = "tap_gesture";
 
         /**
+         * Whether tethering is allowed to use VPN upstreams
+         */
+        public static final String TETHERING_ALLOW_VPN_UPSTREAMS = "tethering_allow_vpn_upstreams";
+
+        /**
          * Controls whether the people strip is enabled.
          * @hide
          */
