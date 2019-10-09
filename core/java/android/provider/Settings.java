dc8110a87f3060d9083e8b56ec3caaedb4907ff0
SuperDroidBond <superdroidbond@yahoo.com>
2019-10-09 16:20:51 +0530

    Network Traffic: Add Option for Users to select size of Single Traffic (Up/Down) Font Size
    Thanks to @SKULSHADY's commit - Havoc-OS/android_frameworks_base@20e6c63#diff-a6b14ee550885fae2c9316575fd6ce9c
    
    Change-Id: If9eef36de9ce6e70cc2b94c920295f30ea40751a

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5231,6 +5231,12 @@ public final class Settings {
         public static final String NETWORK_TRAFFIC_ARROW = "network_traffic_arrow";
 
         /**
+         * What size to show for network traffic indicator in statusbar
+         * @hide
+         */
+         public static final String NETWORK_TRAFFIC_FONT_SIZE = "network_traffic_font_size";
+
+        /**
          * These are all public system settings
          *
          * @hide
