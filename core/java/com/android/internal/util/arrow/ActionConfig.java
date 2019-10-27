GZOSP/frameworks_base

base: gzosp: import gzosp utils
Change-Id: I5a167cbc4bce32a48bd0b65da6b2683daa905784

core/java/com/android/internal/util/gzosp/ActionConfig.java
@@ -1,0 +1,81 @@
+/*
+* Copyright (C) 2014 SlimRoms Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.android.internal.util.gzosp;
+
+public class ActionConfig {
+
+    private String mClickAction;
+    private String mClickActionDescription;
+    private String mLongpressAction;
+    private String mLongpressActionDescription;
+    private String mIconUri;
+
+    public ActionConfig(String clickAction, String clickActionDescription,
+                    String longpressAction, String longpressActionDescription, String iconUri) {
+        mClickAction = clickAction;
+        mClickActionDescription = clickActionDescription;
+        mLongpressAction = longpressAction;
+        mLongpressActionDescription = longpressActionDescription;
+        mIconUri = iconUri;
+    }
+
+    @Override
+    public String toString() {
+        return mClickActionDescription;
+    }
+
+    public String getClickAction() {
+        return mClickAction;
+    }
+
+    public String getClickActionDescription() {
+        return mClickActionDescription;
+    }
+
+    public String getLongpressAction() {
+        return mLongpressAction;
+    }
+
+    public String getLongpressActionDescription() {
+        return mLongpressActionDescription;
+    }
+
+    public String getIcon() {
+        return mIconUri;
+    }
+
+    public void setClickAction(String action) {
+        mClickAction = action;
+    }
+
+    public void setClickActionDescription(String description) {
+        mClickActionDescription = description;
+    }
+
+    public void setLongpressAction(String action) {
+        mLongpressAction = action;
+    }
+
+    public void setLongpressActionDescription(String description) {
+        mLongpressActionDescription = description;
+    }
+
+    public void setIcon(String iconUri) {
+        mIconUri = iconUri;
+    }
+
+}

core/java/com/android/internal/util/gzosp/ConfigSplitHelper.java
@@ -1,0 +1,98 @@
+/*
+* Copyright (C) 2014 SlimRoms Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.android.internal.util.gzosp;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+public class ConfigSplitHelper {
+
+    private static final String SETTINGS_METADATA_NAME = "com.android.settings";
+
+    public static ArrayList<ActionConfig> getActionConfigValues(Context context, String config,
+                String values, String entries, boolean isShortcut) {
+        // init vars to fill with them later the config values
+        int counter = 0;
+        ArrayList<ActionConfig> actionConfigList = new ArrayList<ActionConfig>();
+        ActionConfig actionConfig = null;
+
+        PackageManager pm = context.getPackageManager();
+        Resources settingsResources = null;
+        try {
+            settingsResources = pm.getResourcesForApplication(SETTINGS_METADATA_NAME);
+        } catch (Exception e) {
+            Log.e("ConfigSplitHelper", "can't access settings resources",e);
+        }
+
+        // Split out the config to work with and add to the list
+        for (String configValue : config.split("\\" + ActionConstants.ACTION_DELIMITER)) {
+            counter++;
+            if (counter == 1) {
+                actionConfig = new ActionConfig(configValue,
+                            AppHelper.getProperSummary(context, pm, settingsResources,
+                            configValue, values, entries), null, null, null);
+            }
+            if (counter == 2) {
+                if (isShortcut) {
+                    actionConfig.setIcon(configValue);
+                    actionConfigList.add(actionConfig);
+                    //reset counter due that shortcut iteration of one action is finished
+                    counter = 0;
+                } else {
+                    actionConfig.setLongpressAction(configValue);
+                    actionConfig.setLongpressActionDescription(
+                            AppHelper.getProperSummary(context, pm, settingsResources,
+                            configValue, values, entries));
+                }
+            }
+            if (counter == 3) {
+                actionConfig.setIcon(configValue);
+                actionConfigList.add(actionConfig);
+                //reset counter due that iteration of full config action is finished
+                counter = 0;
+            }
+        }
+
+        return actionConfigList;
+    }
+
+    public static String setActionConfig(
+            ArrayList<ActionConfig> actionConfigs, boolean isShortcut) {
+        String finalConfig = "";
+        ActionConfig actionConfig;
+
+        for (int i = 0; i < actionConfigs.size(); i++) {
+            if (i != 0) {
+                finalConfig += ActionConstants.ACTION_DELIMITER;
+            }
+            actionConfig = actionConfigs.get(i);
+            finalConfig += actionConfig.getClickAction() + ActionConstants.ACTION_DELIMITER;
+            if (!isShortcut) {
+                finalConfig += actionConfig.getLongpressAction()
+                    + ActionConstants.ACTION_DELIMITER;
+            }
+            finalConfig += actionConfig.getIcon();
+        }
+
+        return finalConfig;
+    }
+
+}

core/java/com/android/internal/util/gzosp/Converter.java
@@ -1,0 +1,30 @@
+/*
+* Copyright (C) 2014 SlimRoms Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package com.android.internal.util.gzosp;
+
+import android.content.Context;
+
+public class Converter {
+
+    public static int dpToPx(Context context, int dp) {
+        return (int) ((dp * context.getResources().getDisplayMetrics().density) + 0.5);
+    }
+
+    public static int pxToDp(Context context, int px) {
+        return (int) ((px / context.getResources().getDisplayMetrics().density) + 0.5);
+    }
+
+}

core/java/com/android/internal/util/gzosp/Helpers.java
@@ -1,0 +1,30 @@
+package com.android.internal.util.gzosp;
+
+import android.app.ActivityManager;
+import android.app.ActivityManagerNative;
+import android.app.IActivityManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+
+public class Helpers {
+    // avoids hardcoding the tag
+    private static final String TAG = Thread.currentThread().getStackTrace()[1].getClassName();
+
+    public static void restartSystemUI(Context context) {
+        try {
+            ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+            IActivityManager amn = ActivityManagerNative.getDefault();
+            context.stopService(new Intent().setComponent(new ComponentName("com.android.systemui", "com.android.systemui.SystemUIService")));
+            am.killBackgroundProcesses("com.android.systemui");
+            for (ActivityManager.RunningAppProcessInfo app : am.getRunningAppProcesses()) {
+                if ("com.android.systemui".equals(app.processName)) {
+                    amn.killApplicationProcess(app.processName, app.uid);
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}

core/java/com/android/internal/util/gzosp/KeyDisabler.java
@@ -1,0 +1,53 @@
+/*
+ * Copyright (C) 2014 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.util.gzosp;
+
+/*
+ * Disable capacitive keys
+ *
+ * This is intended for use on devices in which the capacitive keys
+ * can be fully disabled for replacement with a soft navbar. You
+ * really should not be using this on a device with mechanical or
+ * otherwise visible-when-inactive keys
+ */
+
+public class KeyDisabler {
+
+    /*
+     * All HAF classes should export this boolean.
+     * Real implementations must, of course, return true
+     */
+
+    public static boolean isSupported() { return false; }
+
+    /*
+     * Are the keys currently blocked?
+     */
+
+    public static boolean isActive() {
+        return false;
+    }
+
+    /*
+     * Disable capacitive keys
+     */
+
+    public static boolean setActive(boolean state) {
+        throw new UnsupportedOperationException();
+    }
+
+}

ArrowOS/android_frameworks_base

core/java/com/android/internal/util/arrow/ActionConfig.java
@@ -1,0 +1,81 @@
+/*
+* Copyright (C) 2014 SlimRoms Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.android.internal.util.arrow;
+
+public class ActionConfig {
+
+    private String mClickAction;
+    private String mClickActionDescription;
+    private String mLongpressAction;
+    private String mLongpressActionDescription;
+    private String mIconUri;
+
+    public ActionConfig(String clickAction, String clickActionDescription,
+                    String longpressAction, String longpressActionDescription, String iconUri) {
+        mClickAction = clickAction;
+        mClickActionDescription = clickActionDescription;
+        mLongpressAction = longpressAction;
+        mLongpressActionDescription = longpressActionDescription;
+        mIconUri = iconUri;
+    }
+
+    @Override
+    public String toString() {
+        return mClickActionDescription;
+    }
+
+    public String getClickAction() {
+        return mClickAction;
+    }
+
+    public String getClickActionDescription() {
+        return mClickActionDescription;
+    }
+
+    public String getLongpressAction() {
+        return mLongpressAction;
+    }
+
+    public String getLongpressActionDescription() {
+        return mLongpressActionDescription;
+    }
+
+    public String getIcon() {
+        return mIconUri;
+    }
+
+    public void setClickAction(String action) {
+        mClickAction = action;
+    }
+
+    public void setClickActionDescription(String description) {
+        mClickActionDescription = description;
+    }
+
+    public void setLongpressAction(String action) {
+        mLongpressAction = action;
+    }
+
+    public void setLongpressActionDescription(String description) {
+        mLongpressActionDescription = description;
+    }
+
+    public void setIcon(String iconUri) {
+        mIconUri = iconUri;
+    }
+
+}

core/java/com/android/internal/util/arrow/ConfigSplitHelper.java
@@ -1,0 +1,98 @@
+/*
+* Copyright (C) 2014 SlimRoms Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.android.internal.util.arrow;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+public class ConfigSplitHelper {
+
+    private static final String SETTINGS_METADATA_NAME = "com.android.settings";
+
+    public static ArrayList<ActionConfig> getActionConfigValues(Context context, String config,
+                String values, String entries, boolean isShortcut) {
+        // init vars to fill with them later the config values
+        int counter = 0;
+        ArrayList<ActionConfig> actionConfigList = new ArrayList<ActionConfig>();
+        ActionConfig actionConfig = null;
+
+        PackageManager pm = context.getPackageManager();
+        Resources settingsResources = null;
+        try {
+            settingsResources = pm.getResourcesForApplication(SETTINGS_METADATA_NAME);
+        } catch (Exception e) {
+            Log.e("ConfigSplitHelper", "can't access settings resources",e);
+        }
+
+        // Split out the config to work with and add to the list
+        for (String configValue : config.split("\\" + ActionConstants.ACTION_DELIMITER)) {
+            counter++;
+            if (counter == 1) {
+                actionConfig = new ActionConfig(configValue,
+                            AppHelper.getProperSummary(context, pm, settingsResources,
+                            configValue, values, entries), null, null, null);
+            }
+            if (counter == 2) {
+                if (isShortcut) {
+                    actionConfig.setIcon(configValue);
+                    actionConfigList.add(actionConfig);
+                    //reset counter due that shortcut iteration of one action is finished
+                    counter = 0;
+                } else {
+                    actionConfig.setLongpressAction(configValue);
+                    actionConfig.setLongpressActionDescription(
+                            AppHelper.getProperSummary(context, pm, settingsResources,
+                            configValue, values, entries));
+                }
+            }
+            if (counter == 3) {
+                actionConfig.setIcon(configValue);
+                actionConfigList.add(actionConfig);
+                //reset counter due that iteration of full config action is finished
+                counter = 0;
+            }
+        }
+
+        return actionConfigList;
+    }
+
+    public static String setActionConfig(
+            ArrayList<ActionConfig> actionConfigs, boolean isShortcut) {
+        String finalConfig = "";
+        ActionConfig actionConfig;
+
+        for (int i = 0; i < actionConfigs.size(); i++) {
+            if (i != 0) {
+                finalConfig += ActionConstants.ACTION_DELIMITER;
+            }
+            actionConfig = actionConfigs.get(i);
+            finalConfig += actionConfig.getClickAction() + ActionConstants.ACTION_DELIMITER;
+            if (!isShortcut) {
+                finalConfig += actionConfig.getLongpressAction()
+                    + ActionConstants.ACTION_DELIMITER;
+            }
+            finalConfig += actionConfig.getIcon();
+        }
+
+        return finalConfig;
+    }
+
+}

core/java/com/android/internal/util/arrow/Converter.java
@@ -1,0 +1,30 @@
+/*
+* Copyright (C) 2014 SlimRoms Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package com.android.internal.util.arrow;
+
+import android.content.Context;
+
+public class Converter {
+
+    public static int dpToPx(Context context, int dp) {
+        return (int) ((dp * context.getResources().getDisplayMetrics().density) + 0.5);
+    }
+
+    public static int pxToDp(Context context, int px) {
+        return (int) ((px / context.getResources().getDisplayMetrics().density) + 0.5);
+    }
+
+}

core/java/com/android/internal/util/arrow/Helpers.java
@@ -1,0 +1,30 @@
+package com.android.internal.util.arrow;
+
+import android.app.ActivityManager;
+import android.app.ActivityManagerNative;
+import android.app.IActivityManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+
+public class Helpers {
+    // avoids hardcoding the tag
+    private static final String TAG = Thread.currentThread().getStackTrace()[1].getClassName();
+
+    public static void restartSystemUI(Context context) {
+        try {
+            ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+            IActivityManager amn = ActivityManagerNative.getDefault();
+            context.stopService(new Intent().setComponent(new ComponentName("com.android.systemui", "com.android.systemui.SystemUIService")));
+            am.killBackgroundProcesses("com.android.systemui");
+            for (ActivityManager.RunningAppProcessInfo app : am.getRunningAppProcesses()) {
+                if ("com.android.systemui".equals(app.processName)) {
+                    amn.killApplicationProcess(app.processName, app.uid);
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}

core/java/com/android/internal/util/arrow/KeyDisabler.java
@@ -1,0 +1,53 @@
+/*
+ * Copyright (C) 2014 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.util.arrow;
+
+/*
+ * Disable capacitive keys
+ *
+ * This is intended for use on devices in which the capacitive keys
+ * can be fully disabled for replacement with a soft navbar. You
+ * really should not be using this on a device with mechanical or
+ * otherwise visible-when-inactive keys
+ */
+
+public class KeyDisabler {
+
+    /*
+     * All HAF classes should export this boolean.
+     * Real implementations must, of course, return true
+     */
+
+    public static boolean isSupported() { return false; }
+
+    /*
+     * Are the keys currently blocked?
+     */
+
+    public static boolean isActive() {
+        return false;
+    }
+
+    /*
+     * Disable capacitive keys
+     */
+
+    public static boolean setActive(boolean state) {
+        throw new UnsupportedOperationException();
+    }
+
+}