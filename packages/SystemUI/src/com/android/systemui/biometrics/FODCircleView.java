36698e92dab819d144e49fc1b09d09d3b5faab4f
TheScarastic <warabhishek@gmail.com>
2020-01-13 19:50:30 +0000

    FODCircleView: Patch screen flickering
    Change-Id: Ibf8b9131ded3c9ccc3611a4ab05f59445d146fea

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -191,6 +191,17 @@ public class FODCircleView extends ImageView {
     }
 
     @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+
+        if (mIsCircleShowing) {
+            dispatchPress();
+        } else {
+            dispatchRelease();
+        }
+    }
+
+    @Override
     public boolean onTouchEvent(MotionEvent event) {
         float x = event.getAxisValue(MotionEvent.AXIS_X);
         float y = event.getAxisValue(MotionEvent.AXIS_Y);
@@ -273,13 +284,9 @@ public class FODCircleView extends ImageView {
 
         setKeepScreenOn(true);
 
-        if (mIsDreaming) {
-            mWakeLock.acquire(300);
-        }
-
+        if (mIsDreaming) mWakeLock.acquire(500);
         setDim(true);
         updateAlpha();
-        dispatchPress();
 
         setImageDrawable(null);
         invalidate();
@@ -290,8 +297,6 @@ public class FODCircleView extends ImageView {
 
         setImageResource(R.drawable.fod_icon_default);
         invalidate();
-
-        dispatchRelease();
 
         setDim(false);
         updateAlpha();
