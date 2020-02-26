dbfa3593d645006181af52af38408ae60978b36c
kubersharma001 <kubersharma001@gmail.com>
2020-02-26 14:04:30 +0100

    PowerMenu: Dialog should point to where Advanced restart toggle is
    * It should open to the "Enable Advanced restart" toggle
      and not to point us to what to add or remove from
      PowerMenu.
    
    Change-Id: I2daece632b27154045032ce648ef98c6f70032c7

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
@@ -770,7 +770,7 @@ public class GlobalActionsDialog implements DialogInterface.OnDismissListener,
                 public void onClick(DialogInterface dialog, int which) {
                     dialog.dismiss();
                     Intent globalActionSettings = new Intent();
-                    globalActionSettings.setClassName("com.android.settings", "com.android.settings.Settings$GlobalActionsSettingsActivity");
+                    globalActionSettings.setClassName("com.android.settings", "com.android.settings.Settings$HardwareKeys");
                     globalActionSettings.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                     mContext.startActivity(globalActionSettings);
                 }
