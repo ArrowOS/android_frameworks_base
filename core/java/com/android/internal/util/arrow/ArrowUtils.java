3598baa09e344a1d3f758c7d3e29112d69020499
kubersharma001 <kubersharma001@gmail.com>
2020-02-26 16:28:41 +0100

    Utils: Battery temp: Make celsius default.
    Change-Id: I92eb3b6f35af26c973122fe4058bd73762ddde8f

--- core/java/com/android/internal/util/arrow/ArrowUtils.java
+++ core/java/com/android/internal/util/arrow/ArrowUtils.java
@@ -152,7 +152,7 @@ public class ArrowUtils {
         float n = temp + 0.5f;
         // Use boolean to determine celsius or fahrenheit
         return String.valueOf((n - c) % 2 == 0 ? (int) temp :
-                ForC ? c * 9/5 + 32 + "°F" :c + "°C");
+                ForC ? c + "°C" :c * 9/5 + 32 + "°F");
     }
 
     // Check if device has a notch
