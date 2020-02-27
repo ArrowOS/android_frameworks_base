aea3838dcc88d8876944348ea7dbdda783f08717
Pranav Vashi <neobuddy89@gmail.com>
2020-02-28 00:19:59 +0530

    Alert Slider: Add toggle to disable notifications [1/2]
    Change-Id: If7aca1f3f52b338e74f504652ebf16e26d77a8fc
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5250,7 +5250,7 @@ public final class Settings {
          * the setting value. See an example above.
          */
 
-        /**
+	/**
          * Double tap on lockscreen to sleep
          * @hide
          */
@@ -5316,6 +5316,12 @@ public final class Settings {
         * @hide
         */
        public static final String BATTERY_LIGHT_REALLYFULL_COLOR = "battery_light_reallyfull_color";
+
+        /**
+         * Whether to show or hide alert slider notifications on supported devices
+         * @hide
+         */
+        public static final String ALERT_SLIDER_NOTIFICATIONS = "alert_slider_notifications";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
