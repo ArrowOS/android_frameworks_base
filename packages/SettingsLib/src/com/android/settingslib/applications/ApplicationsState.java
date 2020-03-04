8bafcb4a4e756cec39135eef7db7a5966ecf0759
kubersharma001 <kubersharma001@gmail.com>
2020-03-04 10:24:31 +0100

    Revert "SettingsLib: Don't show system overlays on apps list"
    This reverts commit f05e902b2a6cc6c984ac92ececc765e8d668705c.

--- packages/SettingsLib/src/com/android/settingslib/applications/ApplicationsState.java
+++ packages/SettingsLib/src/com/android/settingslib/applications/ApplicationsState.java
@@ -1821,7 +1821,7 @@ public class ApplicationsState {
 
         @Override
         public boolean filterApp(AppEntry entry) {
-            return !hasFlag(entry.info.privateFlags, ApplicationInfo.PRIVATE_FLAG_IS_RESOURCE_OVERLAY);
+            return true;
         }
     };
 
