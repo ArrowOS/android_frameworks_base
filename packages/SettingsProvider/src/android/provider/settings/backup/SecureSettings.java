eff9362636d2cf3b19d40802f5f264419940c063
althafvly <althafvly@gmail.com>
2020-05-26 21:17:59 +0800

    SystemUI: support black theme for dark mode [1/3]
    Co-authored-by: Jesse Chan <jc@lineageos.org>
    Change-Id: I57cd53de8f2c1e4d445441b514875b6af915b858
    
    -----
    
    Don't skip theme application if it's already current
    
    This fixes black theme and font/icon shape customizations.
    
    Change-Id: I8c3d3b4634e1ac60d28e4d5ef20bf9c2015e2e18
    Co-authored-by: Jesse Chan <jc@lineageos.org>
    Co-authored-by: Michael Bestas <mkbestas@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -216,6 +216,7 @@ public class SecureSettings {
         Settings.Secure.BLUETOOTH_LE_BROADCAST_PROGRAM_INFO,
         Settings.Secure.BLUETOOTH_LE_BROADCAST_CODE,
         Settings.Secure.BLUETOOTH_LE_BROADCAST_APP_SOURCE_NAME,
-        Settings.Secure.TETHERING_ALLOW_VPN_UPSTREAMS
+        Settings.Secure.TETHERING_ALLOW_VPN_UPSTREAMS,
+        Settings.Secure.BERRY_BLACK_THEME
     };
 }
