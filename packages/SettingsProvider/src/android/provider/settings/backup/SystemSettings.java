d9c22cf2ab159a0f3d31be291c83ccd23de554b5
DennySPb <dennyspb@gmail.com>
2020-06-22 16:52:51 +0300

    SystemUI: Optional VoWiFi icon
    [spezi77] Put into a single commit to use AOSP tuner instead of 2 parts
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Change-Id: I637cbc81283175cdb092a4e70fda28cacde7238c

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -89,5 +89,6 @@ public class SystemSettings {
         Settings.System.NETWORK_TRAFFIC_STATE,
         Settings.System.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD,
         Settings.System.SHOW_VOLTE_ICON,
+        Settings.System.SHOW_VOWIFI_ICON,
     };
 }
