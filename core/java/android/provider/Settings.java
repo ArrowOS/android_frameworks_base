20da3fac098d18bd34f3344f477cb2b4af523d30
maxwen <max.weninger@gmail.com>
2020-10-05 11:57:31 +0200

    SystemUI: qs column settings
    add
      QS_LAYOUT_COLUMNS_LANDSCAPE,
      QS_LAYOUT_COLUMNS,
      QS_QUICKBAR_COLUMNS,
      QS_TILE_TITLE_VISIBILITY,
    
    does NOT contain any ui to change - this will be done extra
    
    Change-Id: Ia5c182ff9e041895e194475cb91cc9a639f7bf3b

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5068,6 +5068,27 @@ public final class Settings {
         public static final String LOCKSCREEN_WEATHER_STYLE = "lockscreen_weather_style";
 
         /**
+         * @hide
+         */
+         public static final String QS_LAYOUT_COLUMNS_LANDSCAPE = "qs_layout_columns_landscape";
+
+         /**
+         * @hide
+         */
+        public static final String QS_LAYOUT_COLUMNS = "qs_layout_columns";
+
+        /**
+         * @hide
+         */
+        public static final String QS_QUICKBAR_COLUMNS = "qs_quickbar_columns";
+
+        /**
+         * Whether to display qs tile titles in the qs panel
+         * @hide
+         */
+        public static final String QS_TILE_TITLE_VISIBILITY = "qs_tile_title_visibility";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
