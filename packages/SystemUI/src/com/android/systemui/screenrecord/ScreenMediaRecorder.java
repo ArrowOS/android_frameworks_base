160655995ad53935c42bd5cd73dc7065183d8ab2
Wang Han <wanghan1995315@gmail.com>
2020-10-11 12:25:17 +0000

    [REAPPLY]SystemUI: Relax AVC level requirement
     * Android only forces AVCLevel 3, and it is common for modern devices
       to use AVCLevel 4.2 and 3.2 Considered that there is no checks if this level
       is supported, downgrade the requirement to get screen recording work
       on older devices.
    
     * We had this commit with original change-id: d02c24f99bc5c9eba58c4011e2facdcaf29de785, but was drop in 87881e8afa96a371ce0e28817c4914a9a24b3048.
    
    Change-Id: I31d87b7f64dea0cc5fab2e93723f03afb7fcb6e0

--- packages/SystemUI/src/com/android/systemui/screenrecord/ScreenMediaRecorder.java
+++ packages/SystemUI/src/com/android/systemui/screenrecord/ScreenMediaRecorder.java
@@ -1,18 +1,6 @@
-/*
- * Copyright (C) 2020 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+/* * Copyright (C) 2020 The Android Open Source Project * * Licensed under the Apache License, Version 2.0 (the "License"); * you may not use this file except in compliance with the License. * You may 
+ obtain a copy of the License at * * http://www.apache.org/licenses/LICENSE-2.0 * * Unless required by applicable law or agreed to in writing, software * distributed under the License is distributed on 
+ an "AS IS" BASIS, * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. * See the License for the specific language governing permissions and * limitations under the License. */
 
 package com.android.systemui.screenrecord;
 
@@ -149,9 +137,8 @@ public class ScreenMediaRecorder {
         but at the same time the cpu load is 8-10 times higher and some devices don't support it yet */
         mMediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264);
         mMediaRecorder.setVideoEncodingProfileLevel(
-                MediaCodecInfo.CodecProfileLevel.AVCProfileMain,/*actually the common used main level profile*/
-                mLowQuality ? MediaCodecInfo.CodecProfileLevel.AVCLevel32/*level 3.2*/
-                : MediaCodecInfo.CodecProfileLevel.AVCLevel42/*level 4.2*/);
+                MediaCodecInfo.CodecProfileLevel.AVCProfileMain,
+                MediaCodecInfo.CodecProfileLevel.AVCLevel3);
         mMediaRecorder.setVideoSize(screenWidth, screenHeight);
         mMediaRecorder.setVideoFrameRate(refereshRate);
         mMediaRecorder.setVideoEncodingBitRate(vidBitRate);
