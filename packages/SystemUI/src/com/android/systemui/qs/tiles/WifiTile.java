57e0c8f2e3a2aa015c460aee41cf4f8be6c984b1
xyyx <xyyx@mail.ru>
2020-10-20 08:42:37 +0300

    WifiTile: Fix detail view after requiring unlock commit
    Change-Id: If445ccb26da004dedd3d7151731481950dc058e1

--- packages/SystemUI/src/com/android/systemui/qs/tiles/WifiTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/WifiTile.java
@@ -167,6 +167,7 @@ public class WifiTile extends QSTileImpl<SignalState> {
             });
             return;
         }
+        showDetail(true);
         if (!mState.value) {
             mController.setWifiEnabled(true);
         }
