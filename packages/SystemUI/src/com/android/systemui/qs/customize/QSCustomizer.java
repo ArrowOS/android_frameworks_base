omnirom/android_frameworks_base_old

SystemUI: new qs config UI
instead of context menu show a settings view on top

TODO: use view pager with tabs for other settings
that we had in tuner last time

Change-Id: I45cd7afeae2ea3e18209582f23e149cc5411eece

packages/SystemUI/src/com/android/systemui/qs/customize/QSCustomizer.java
@@ -18,6 +18,7 @@ package com.android.systemui.qs.customize;
 import android.animation.Animator;
 import android.animation.Animator.AnimatorListener;
 import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.content.res.Configuration;
 import android.os.Bundle;
@@ -29,6 +30,7 @@ import android.util.TypedValue;
 import android.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
@@ -70,7 +72,6 @@ import javax.inject.Inject;
 public class QSCustomizer extends LinearLayout implements OnMenuItemClickListener,
         OmniSettingsService.OmniSettingsObserver {
 
-    private static final int MENU_RESET = Menu.FIRST;
     private static final String EXTRA_QS_CUSTOMIZING = "qs_customizing";
     private static final String TAG = "QSCustomizer";
     private static final boolean DEBUG = false;
@@ -100,6 +101,8 @@ public class QSCustomizer extends LinearLayout implements OnMenuItemClickListene
     private int mCellMarginHorizontal;
     private int mColumns = 3;
     private boolean mShowLabels = true;
+    private boolean mSettingsShown;
+    private int mOrientation = -1;
 
     @Inject
     public QSCustomizer(Context context, AttributeSet attrs,
@@ -124,8 +127,10 @@ public class QSCustomizer extends LinearLayout implements OnMenuItemClickListene
             }
         });
         mToolbar.setOnMenuItemClickListener(this);
-        mToolbar.getMenu().add(Menu.NONE, MENU_RESET, 0,
-                mContext.getString(com.android.internal.R.string.reset));
+
+        MenuInflater menuInflater = new MenuInflater(mContext);
+        menuInflater.inflate(R.menu.qs_customize_menu, mToolbar.getMenu());
+
         mToolbar.setTitle(R.string.qs_edit);
         mRecyclerView = findViewById(android.R.id.list);
         mTransparentView = findViewById(R.id.customizer_transparent_view);
@@ -153,6 +158,7 @@ public class QSCustomizer extends LinearLayout implements OnMenuItemClickListene
         mLightBarController = lightBarController;
         mKeyguardStateController = keyguardStateController;
         mScreenLifecycle = screenLifecycle;
+        mOrientation = getResources().getConfiguration().orientation;
         updateNavBackDrop(getResources().getConfiguration());
     }
 
@@ -161,6 +167,11 @@ public class QSCustomizer extends LinearLayout implements OnMenuItemClickListene
         super.onConfigurationChanged(newConfig);
         updateNavBackDrop(newConfig);
         updateResources();
+        if (mOrientation != newConfig.orientation) {
+            // force reinflate of settings - needed if orientation changed
+            mRecyclerView.setAdapter(mTileAdapter);
+        }
+        mOrientation = newConfig.orientation;
     }
 
     private void updateResources() {
@@ -203,6 +214,8 @@ public class QSCustomizer extends LinearLayout implements OnMenuItemClickListene
      */
     public void show(int x, int y) {
         if (!isShown) {
+            mTileAdapter.setShowSettings(false);
+            mSettingsShown = false;
             int containerLocation[] = findViewById(R.id.customize_container).getLocationOnScreen();
             mX = x - containerLocation[0];
             mY = y - containerLocation[1];
@@ -224,6 +237,8 @@ public class QSCustomizer extends LinearLayout implements OnMenuItemClickListene
 
     public void showImmediately() {
         if (!isShown) {
+            mTileAdapter.setShowSettings(false);
+            mSettingsShown = false;
             setVisibility(VISIBLE);
             mClipper.cancelAnimator();
             mClipper.showBackground();
@@ -283,11 +298,18 @@ public class QSCustomizer extends LinearLayout implements OnMenuItemClickListene
 
     @Override
     public boolean onMenuItemClick(MenuItem item) {
-        switch (item.getItemId()) {
-            case MENU_RESET:
-                mUiEventLogger.log(QSEditEvent.QS_EDIT_RESET);
-                reset();
-                break;
+        int id = item.getItemId();
+        if (id == R.id.menu_item_reset) {
+            mUiEventLogger.log(QSEditEvent.QS_EDIT_RESET);
+            reset();
+        } else if (id == R.id.menu_item_settings) {
+            if (mSettingsShown) {
+                mTileAdapter.setShowSettings(false);
+                mSettingsShown = false;
+            } else {
+                mTileAdapter.setShowSettings(true);
+                mSettingsShown = true;
+            }
         }
         return false;
     }
@@ -391,6 +413,7 @@ public class QSCustomizer extends LinearLayout implements OnMenuItemClickListene
     private void attachSettings() {
         Dependency.get(OmniSettingsService.class).addIntObserver(this, Settings.System.OMNI_QS_LAYOUT_COLUMNS);
         Dependency.get(OmniSettingsService.class).addIntObserver(this, Settings.System.OMNI_QS_LAYOUT_COLUMNS_LANDSCAPE);
+        Dependency.get(OmniSettingsService.class).addIntObserver(this, Settings.System.OMNI_QS_QUICKBAR_COLUMNS);
         Dependency.get(OmniSettingsService.class).addIntObserver(this, Settings.System.OMNI_QS_TILE_TITLE_VISIBILITY);
     }
 

packages/SystemUI/src/com/android/systemui/qs/customize/QSCustomizerSettings.java
@@ -1,0 +1,130 @@
+/*
+ * Copyright (C) 2020 The OmniROM Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.systemui.qs.customize;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.ContextThemeWrapper;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.SeekBar;
+import android.widget.Switch;
+
+import com.android.systemui.R;
+
+public class QSCustomizerSettings extends LinearLayout {
+    private static final String TAG = "QSCustomizer::QSCustomizerSettings";
+    private static final boolean DEBUG = false;
+
+    public QSCustomizerSettings(Context context, AttributeSet attrs) {
+        super(new ContextThemeWrapper(context, R.style.edit_theme), attrs);
+    }
+    
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        
+        Switch showLabels = findViewById(R.id.qs_customize_settings_show_labels);
+        boolean showLabelsValue = Settings.System.getIntForUser(
+                mContext.getContentResolver(), Settings.System.OMNI_QS_TILE_TITLE_VISIBILITY, 1,
+                UserHandle.USER_CURRENT) == 1;
+        showLabels.setChecked(showLabelsValue);
+        showLabels.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            Settings.System.putIntForUser(mContext.getContentResolver(),
+                    Settings.System.OMNI_QS_TILE_TITLE_VISIBILITY, isChecked ? 1 : 0,
+                    UserHandle.USER_CURRENT);
+        });
+        int defaultMaxTiles = mContext.getResources().getInteger(R.integer.quick_qs_panel_max_columns);
+        int quickColumns = Settings.System.getIntForUser(
+                mContext.getContentResolver(), Settings.System.OMNI_QS_QUICKBAR_COLUMNS,
+                defaultMaxTiles, UserHandle.USER_CURRENT);
+        final SeekBar quickColumnsSlider = findViewById(R.id.qs_customize_settings_quickbar);
+        Switch quickFollow = findViewById(R.id.qs_customize_settings_quickbar_follow);
+        quickFollow.setChecked(quickColumns == -1);
+        quickFollow.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            Settings.System.putIntForUser(mContext.getContentResolver(),
+                    Settings.System.OMNI_QS_QUICKBAR_COLUMNS, isChecked ? -1 : defaultMaxTiles,
+                    UserHandle.USER_CURRENT);
+            quickColumnsSlider.setEnabled(!isChecked);
+        });
+        quickColumnsSlider.setProgress(quickColumns != -1 ? quickColumns : defaultMaxTiles);
+        quickColumnsSlider.setEnabled(quickColumns != -1);
+        quickColumnsSlider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser) {
+                    Settings.System.putIntForUser(mContext.getContentResolver(),
+                            Settings.System.OMNI_QS_QUICKBAR_COLUMNS, progress,
+                            UserHandle.USER_CURRENT);
+                }
+            }
+
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
+        int resourceColumns = Math.max(1, mContext.getResources().getInteger(R.integer.quick_settings_num_columns));
+        int columnsPort = Settings.System.getIntForUser(
+                mContext.getContentResolver(), Settings.System.OMNI_QS_LAYOUT_COLUMNS,
+                resourceColumns, UserHandle.USER_CURRENT);
+        SeekBar columnsSliderPort = findViewById(R.id.qs_customize_settings_columns_port);
+        columnsSliderPort.setProgress(columnsPort);
+        columnsSliderPort.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser) {
+                    Settings.System.putIntForUser(mContext.getContentResolver(),
+                            Settings.System.OMNI_QS_LAYOUT_COLUMNS, progress,
+                            UserHandle.USER_CURRENT);
+                }
+            }
+
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+        int columnsLand = Settings.System.getIntForUser(
+                mContext.getContentResolver(), Settings.System.OMNI_QS_LAYOUT_COLUMNS_LANDSCAPE,
+                resourceColumns, UserHandle.USER_CURRENT);
+        SeekBar columnsSliderLand = findViewById(R.id.qs_customize_settings_columns_land);
+        columnsSliderLand.setProgress(columnsLand);
+        columnsSliderLand.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser) {
+                    Settings.System.putIntForUser(mContext.getContentResolver(),
+                            Settings.System.OMNI_QS_LAYOUT_COLUMNS_LANDSCAPE, progress,
+                            UserHandle.USER_CURRENT);
+                }
+            }
+
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+    }
+}

ArrowOS/android_frameworks_base

packages/SystemUI/src/com/android/systemui/qs/customize/QSCustomizer.java
@@ -18,6 +18,7 @@ package com.android.systemui.qs.customize;
 import android.animation.Animator;
 import android.animation.Animator.AnimatorListener;
 import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.content.res.Configuration;
 import android.os.Bundle;
@@ -29,6 +30,7 @@ import android.util.TypedValue;
 import android.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
@@ -70,7 +72,6 @@ import javax.inject.Inject;
 public class QSCustomizer extends LinearLayout implements OnMenuItemClickListener,
         ArrowSettingsService.ArrowSettingsObserver {
 
-    private static final int MENU_RESET = Menu.FIRST;
     private static final String EXTRA_QS_CUSTOMIZING = "qs_customizing";
     private static final String TAG = "QSCustomizer";
     private static final boolean DEBUG = false;
@@ -100,6 +101,8 @@ public class QSCustomizer extends LinearLayout implements OnMenuItemClickListene
     private int mCellMarginHorizontal;
     private int mColumns = 3;
     private boolean mShowLabels = true;
+    private boolean mSettingsShown;
+    private int mOrientation = -1;
 
     @Inject
     public QSCustomizer(Context context, AttributeSet attrs,
@@ -124,8 +127,10 @@ public class QSCustomizer extends LinearLayout implements OnMenuItemClickListene
             }
         });
         mToolbar.setOnMenuItemClickListener(this);
-        mToolbar.getMenu().add(Menu.NONE, MENU_RESET, 0,
-                mContext.getString(com.android.internal.R.string.reset));
+
+        MenuInflater menuInflater = new MenuInflater(mContext);
+        menuInflater.inflate(R.menu.qs_customize_menu, mToolbar.getMenu());
+
         mToolbar.setTitle(R.string.qs_edit);
         mRecyclerView = findViewById(android.R.id.list);
         mTransparentView = findViewById(R.id.customizer_transparent_view);
@@ -153,6 +158,7 @@ public class QSCustomizer extends LinearLayout implements OnMenuItemClickListene
         mLightBarController = lightBarController;
         mKeyguardStateController = keyguardStateController;
         mScreenLifecycle = screenLifecycle;
+        mOrientation = getResources().getConfiguration().orientation;
         updateNavBackDrop(getResources().getConfiguration());
     }
 
@@ -161,6 +167,11 @@ public class QSCustomizer extends LinearLayout implements OnMenuItemClickListene
         super.onConfigurationChanged(newConfig);
         updateNavBackDrop(newConfig);
         updateResources();
+        if (mOrientation != newConfig.orientation) {
+            // force reinflate of settings - needed if orientation changed
+            mRecyclerView.setAdapter(mTileAdapter);
+        }
+        mOrientation = newConfig.orientation;
     }
 
     private void updateResources() {
@@ -203,6 +214,8 @@ public class QSCustomizer extends LinearLayout implements OnMenuItemClickListene
      */
     public void show(int x, int y) {
         if (!isShown) {
+            mTileAdapter.setShowSettings(false);
+            mSettingsShown = false;
             int containerLocation[] = findViewById(R.id.customize_container).getLocationOnScreen();
             mX = x - containerLocation[0];
             mY = y - containerLocation[1];
@@ -224,6 +237,8 @@ public class QSCustomizer extends LinearLayout implements OnMenuItemClickListene
 
     public void showImmediately() {
         if (!isShown) {
+            mTileAdapter.setShowSettings(false);
+            mSettingsShown = false;
             setVisibility(VISIBLE);
             mClipper.cancelAnimator();
             mClipper.showBackground();
@@ -283,11 +298,18 @@ public class QSCustomizer extends LinearLayout implements OnMenuItemClickListene
 
     @Override
     public boolean onMenuItemClick(MenuItem item) {
-        switch (item.getItemId()) {
-            case MENU_RESET:
-                mUiEventLogger.log(QSEditEvent.QS_EDIT_RESET);
-                reset();
-                break;
+        int id = item.getItemId();
+        if (id == R.id.menu_item_reset) {
+            mUiEventLogger.log(QSEditEvent.QS_EDIT_RESET);
+            reset();
+        } else if (id == R.id.menu_item_settings) {
+            if (mSettingsShown) {
+                mTileAdapter.setShowSettings(false);
+                mSettingsShown = false;
+            } else {
+                mTileAdapter.setShowSettings(true);
+                mSettingsShown = true;
+            }
         }
         return false;
     }
@@ -391,6 +413,7 @@ public class QSCustomizer extends LinearLayout implements OnMenuItemClickListene
     private void attachSettings() {
         Dependency.get(ArrowSettingsService.class).addIntObserver(this, Settings.System.QS_LAYOUT_COLUMNS);
         Dependency.get(ArrowSettingsService.class).addIntObserver(this, Settings.System.QS_LAYOUT_COLUMNS_LANDSCAPE);
+        Dependency.get(ArrowSettingsService.class).addIntObserver(this, Settings.System.QS_QUICKBAR_COLUMNS);
         Dependency.get(ArrowSettingsService.class).addIntObserver(this, Settings.System.QS_TILE_TITLE_VISIBILITY);
     }
 

packages/SystemUI/src/com/android/systemui/qs/customize/QSCustomizerSettings.java
@@ -1,0 +1,130 @@
+/*
+ * Copyright (C) 2020 The OmniROM Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.systemui.qs.customize;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.ContextThemeWrapper;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.SeekBar;
+import android.widget.Switch;
+
+import com.android.systemui.R;
+
+public class QSCustomizerSettings extends LinearLayout {
+    private static final String TAG = "QSCustomizer::QSCustomizerSettings";
+    private static final boolean DEBUG = false;
+
+    public QSCustomizerSettings(Context context, AttributeSet attrs) {
+        super(new ContextThemeWrapper(context, R.style.edit_theme), attrs);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+
+        Switch showLabels = findViewById(R.id.qs_customize_settings_show_labels);
+        boolean showLabelsValue = Settings.System.getIntForUser(
+                mContext.getContentResolver(), Settings.System.QS_TILE_TITLE_VISIBILITY, 1,
+                UserHandle.USER_CURRENT) == 1;
+        showLabels.setChecked(showLabelsValue);
+        showLabels.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            Settings.System.putIntForUser(mContext.getContentResolver(),
+                    Settings.System.QS_TILE_TITLE_VISIBILITY, isChecked ? 1 : 0,
+                    UserHandle.USER_CURRENT);
+        });
+        int defaultMaxTiles = mContext.getResources().getInteger(R.integer.quick_qs_panel_max_columns);
+        int quickColumns = Settings.System.getIntForUser(
+                mContext.getContentResolver(), Settings.System.QS_QUICKBAR_COLUMNS,
+                defaultMaxTiles, UserHandle.USER_CURRENT);
+        final SeekBar quickColumnsSlider = findViewById(R.id.qs_customize_settings_quickbar);
+        Switch quickFollow = findViewById(R.id.qs_customize_settings_quickbar_follow);
+        quickFollow.setChecked(quickColumns == -1);
+        quickFollow.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            Settings.System.putIntForUser(mContext.getContentResolver(),
+                    Settings.System.QS_QUICKBAR_COLUMNS, isChecked ? -1 : defaultMaxTiles,
+                    UserHandle.USER_CURRENT);
+            quickColumnsSlider.setEnabled(!isChecked);
+        });
+        quickColumnsSlider.setProgress(quickColumns != -1 ? quickColumns : defaultMaxTiles);
+        quickColumnsSlider.setEnabled(quickColumns != -1);
+        quickColumnsSlider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser) {
+                    Settings.System.putIntForUser(mContext.getContentResolver(),
+                            Settings.System.QS_QUICKBAR_COLUMNS, progress,
+                            UserHandle.USER_CURRENT);
+                }
+            }
+
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
+        int resourceColumns = Math.max(1, mContext.getResources().getInteger(R.integer.quick_settings_num_columns));
+        int columnsPort = Settings.System.getIntForUser(
+                mContext.getContentResolver(), Settings.System.QS_LAYOUT_COLUMNS,
+                resourceColumns, UserHandle.USER_CURRENT);
+        SeekBar columnsSliderPort = findViewById(R.id.qs_customize_settings_columns_port);
+        columnsSliderPort.setProgress(columnsPort);
+        columnsSliderPort.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser) {
+                    Settings.System.putIntForUser(mContext.getContentResolver(),
+                            Settings.System.QS_LAYOUT_COLUMNS, progress,
+                            UserHandle.USER_CURRENT);
+                }
+            }
+
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+        int columnsLand = Settings.System.getIntForUser(
+                mContext.getContentResolver(), Settings.System.QS_LAYOUT_COLUMNS_LANDSCAPE,
+                resourceColumns, UserHandle.USER_CURRENT);
+        SeekBar columnsSliderLand = findViewById(R.id.qs_customize_settings_columns_land);
+        columnsSliderLand.setProgress(columnsLand);
+        columnsSliderLand.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser) {
+                    Settings.System.putIntForUser(mContext.getContentResolver(),
+                            Settings.System.QS_LAYOUT_COLUMNS_LANDSCAPE, progress,
+                            UserHandle.USER_CURRENT);
+                }
+            }
+
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+    }
+}