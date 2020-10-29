Octavi-OS/platform_frameworks_base

Add Reboot/Recovery QS Tile
Vectorized Reboot/Recovery QS tile drawables Alex Cruz <du.alexcruz@gmail.com>
- Resized but reused the power menu reboot icon to keep things uniformed
- Created a recovery vector that fits the same style too

mydongistiny: Updated priv-app whitelist to fix boot

Change-Id: I0762ffb2b3ddf8c833662a237bde20581e8cc614
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
Signed-off-by: Saikiran <bjsaikiran@gmail.com>
Signed-off-by: Sagarrokade006 <sagarrokade006@gmail.com>

packages/SystemUI/src/com/android/systemui/qs/tiles/RebootTile.java
@@ -1,0 +1,95 @@
+/*
+ * Copyright (C) 2019 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.qs.tiles;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.PowerManager;
+
+import com.android.internal.logging.MetricsLogger;
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.systemui.R;
+import com.android.systemui.plugins.qs.QSTile.BooleanState;
+import com.android.systemui.qs.QSHost;
+import com.android.systemui.qs.tileimpl.QSTileImpl;
+
+import javax.inject.Inject;
+
+public class RebootTile extends QSTileImpl<BooleanState> {
+
+    private boolean mRebootToRecovery = false;
+
+    @Inject
+    public RebootTile(QSHost host) {
+        super(host);
+    }
+
+    @Override
+    public BooleanState newTileState() {
+        return new BooleanState();
+    }
+
+    @Override
+    public void handleClick() {
+        mRebootToRecovery = !mRebootToRecovery;
+        refreshState();
+    }
+
+    @Override
+    protected void handleLongClick() {
+        mHost.collapsePanels();
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                PowerManager pm =
+                    (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
+                pm.reboot(mRebootToRecovery ? "recovery" : "");
+            }
+        }, 500);
+    }
+
+    @Override
+    public Intent getLongClickIntent() {
+        return null;
+    }
+
+    @Override
+    public CharSequence getTileLabel() {
+        return mContext.getString(R.string.quick_settings_reboot_label);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.OCTAVI;
+    }
+
+    @Override
+    protected void handleUpdateState(BooleanState state, Object arg) {
+        if (mRebootToRecovery) {
+            state.label = mContext.getString(R.string.quick_settings_reboot_recovery_label);
+            state.icon = ResourceIcon.get(R.drawable.ic_qs_reboot_recovery);
+        } else {
+            state.label = mContext.getString(R.string.quick_settings_reboot_label);
+            state.icon = ResourceIcon.get(R.drawable.ic_qs_reboot);
+        }
+    }
+
+    @Override
+    public void handleSetListening(boolean listening) {
+    }
+}

ArrowOS/android_frameworks_base

packages/SystemUI/src/com/android/systemui/qs/tiles/RebootTile.java
@@ -1,0 +1,95 @@
+/*
+ * Copyright (C) 2019 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.qs.tiles;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.PowerManager;
+
+import com.android.internal.logging.MetricsLogger;
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.systemui.R;
+import com.android.systemui.plugins.qs.QSTile.BooleanState;
+import com.android.systemui.qs.QSHost;
+import com.android.systemui.qs.tileimpl.QSTileImpl;
+
+import javax.inject.Inject;
+
+public class RebootTile extends QSTileImpl<BooleanState> {
+
+    private boolean mRebootToRecovery = false;
+
+    @Inject
+    public RebootTile(QSHost host) {
+        super(host);
+    }
+
+    @Override
+    public BooleanState newTileState() {
+        return new BooleanState();
+    }
+
+    @Override
+    public void handleClick() {
+        mRebootToRecovery = !mRebootToRecovery;
+        refreshState();
+    }
+
+    @Override
+    protected void handleLongClick() {
+        mHost.collapsePanels();
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                PowerManager pm =
+                    (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
+                pm.reboot(mRebootToRecovery ? "recovery" : "");
+            }
+        }, 500);
+    }
+
+    @Override
+    public Intent getLongClickIntent() {
+        return null;
+    }
+
+    @Override
+    public CharSequence getTileLabel() {
+        return mContext.getString(R.string.quick_settings_reboot_label);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.ARROW;
+    }
+
+    @Override
+    protected void handleUpdateState(BooleanState state, Object arg) {
+        if (mRebootToRecovery) {
+            state.label = mContext.getString(R.string.quick_settings_reboot_recovery_label);
+            state.icon = ResourceIcon.get(R.drawable.ic_qs_reboot_recovery);
+        } else {
+            state.label = mContext.getString(R.string.quick_settings_reboot_label);
+            state.icon = ResourceIcon.get(R.drawable.ic_qs_reboot);
+        }
+    }
+
+    @Override
+    public void handleSetListening(boolean listening) {
+    }
+}