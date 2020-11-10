9b23dac96293f938739601fc383ffec98d3f68ff
kubersharma001 <kubersharma001@gmail.com>
2020-11-10 18:57:00 +0100

    Make "Hide fod while going to sleep" configurable
    https://github.com/ArrowOS/android_frameworks_base/blob/f91230dd1af92ac7455ab9a80e1a3aa4bdbc734b/packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java#L155 fixes High brightness flicker while going to sleep from lockscreen on raphael BUT it causes the FOD icon to glitch out while on AOD on davinci. Hence make this configurable via an overlay to restore proper behaviour for both device's FOD impl.
    
    test:
    On raphael (prebuilt OEM vendor|kernel):
    set the overlay to true on raphael, set FOD, lock screen and press power button and notice that brightness doesn't jump to 100% while turning the screen off
    
    On davinci (OSS vendor|kernel):
    by default the overlay is set to false, set FOD on davinci, enable AOD and try to lock the phone with aod, unlock without fingerprint (power button), lock again and FOD icon will not glitch out like before
    
    Change-Id: I84ce84b8d235de9b8407683900a23f9b404ee8a9

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -64,6 +64,7 @@ public class FODCircleView extends ImageView {
     private final int mSize;
     private final int mDreamingMaxOffset;
     private final int mNavigationBarSize;
+    private final boolean mDeviceFlickersGoingToSleep;
     private final boolean mShouldBoostBrightness;
     private final Paint mPaintFingerprintBackground = new Paint();
     private final Paint mPaintFingerprint = new Paint();
@@ -153,7 +154,16 @@ public class FODCircleView extends ImageView {
 
         @Override
         public void onStartedGoingToSleep(int why) {
-            hide();
+            if (mDeviceFlickersGoingToSleep) {
+                hide();
+            }
+        }
+
+        @Override
+        public void onScreenTurnedOff() {
+            if (!mDeviceFlickersGoingToSleep) {
+                hide();
+            }
         }
 
         @Override
@@ -200,6 +210,9 @@ public class FODCircleView extends ImageView {
         mColorBackground = res.getColor(R.color.config_fodColorBackground);
         mPaintFingerprintBackground.setColor(mColorBackground);
         mPaintFingerprintBackground.setAntiAlias(true);
+
+        mDeviceFlickersGoingToSleep = mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_inDisplayFingerprintFlickersGoingToSleep);
 
         mWindowManager = context.getSystemService(WindowManager.class);
 
