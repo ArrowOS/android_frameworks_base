2189f6d195d9a338ee5636cbf06db978846bb492
daniml3 <danimoral1001@gmail.com>
2020-11-14 14:59:16 +0100

    Refactor how FOD Animation is hidden
    On some devices, FOD does not work on AOD if FOD Animations are disabled.
    This commit makes the FOD Animation to be shown always, but when the animations are disabled, the animation's opacity will be set to 0, which will make them invisible and FOD will be still working on AOD.
    
    test: disable FOD animations, verify that they are invisible and that FOD still works, enable FOD animations, verify they are shown and FOD still works.
    
    Signed-off-by: daniml3 <danimoral1001@gmail.com>
    Change-Id: Idc7351d540684e4dd8d425738ee73b63a755d476

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -308,9 +308,7 @@ public class FODCircleView extends ImageView {
 
         if (event.getAction() == MotionEvent.ACTION_DOWN && newIsInside) {
             showCircle();
-            if (mIsRecognizingAnimEnabled) {
-                mHandler.post(() -> mFODAnimation.showFODanimation());
-            }
+            mHandler.post(() -> mFODAnimation.showFODanimation());
             return true;
         } else if (event.getAction() == MotionEvent.ACTION_UP) {
             hideCircle();
@@ -437,8 +435,9 @@ public class FODCircleView extends ImageView {
     private void updateStyle() {
         mIsRecognizingAnimEnabled = Settings.System.getInt(mContext.getContentResolver(),
                 Settings.System.FOD_RECOGNIZING_ANIMATION, 0) != 0;
+
         if (mFODAnimation != null) {
-            mFODAnimation.update();
+            mFODAnimation.update(mIsRecognizingAnimEnabled);
         }
     }
 
