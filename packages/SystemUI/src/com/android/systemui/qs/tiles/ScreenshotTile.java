6e5dcb2801388397e6e021a8c0dc19d61b90fbde
ezio84 <brabus84@gmail.com>
2020-11-29 10:26:01 +0100

    ScreenshotTile: Improve code
    Change-Id: Ie130e84971cf8f42a52350d7bbabfb1d9feb52ad

--- packages/SystemUI/src/com/android/systemui/qs/tiles/ScreenshotTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/ScreenshotTile.java
@@ -88,10 +88,9 @@ public class ScreenshotTile extends QSTileImpl<BooleanState> {
         mHost.collapsePanels();
 
         //finish collapsing the panel
-        try {
-             Thread.sleep(1000); //1s
-        } catch (InterruptedException ie) {}
-        ArrowUtils.takeScreenshot(mRegion ? false : true);
+        mUiHandler.postDelayed(() -> {
+            ArrowUtils.takeScreenshot(mRegion ? false : true);
+        }, 1000/*timeout*/);
     }
 
     @Override
