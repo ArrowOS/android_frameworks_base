c4a3ddb5d79555ea3cb301a2f2b81b1d01a0b0d4
dataoutputstream <mb150996@gmail.com>
2020-12-13 14:54:41 +0100

    FODCircleView: Set dim behind fod base layer
    * This will fix dimming on Xiaomi Displayfeature FOD implementation
    
    Change-Id: Ia29e048f09fb5293e8ca77d36e2a2707f563459b

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -228,7 +228,8 @@ public class FODCircleView extends ImageView {
         mParams.packageName = "android";
         mParams.type = WindowManager.LayoutParams.TYPE_DISPLAY_OVERLAY;
         mParams.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE |
-                WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN;
+                WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN |
+                WindowManager.LayoutParams.FLAG_DIM_BEHIND;
         mParams.gravity = Gravity.TOP | Gravity.LEFT;
 
         mPressedParams.copyFrom(mParams);
@@ -236,6 +237,8 @@ public class FODCircleView extends ImageView {
 
         mParams.setTitle("Fingerprint on display");
         mPressedParams.setTitle("Fingerprint on display.touched");
+
+        mParams.dimAmount = 0.0f;
 
         mPressedView = new ImageView(context)  {
             @Override
