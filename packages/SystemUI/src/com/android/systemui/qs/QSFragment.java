b13048a18b3961cf9d88048367b504b1a39fe9f0
daniml3 <danimoral1001@gmail.com>
2021-02-24 22:09:30 +0100

    SystemUI: update the QS resources when updating the QS state
    Signed-off-by: daniml3 <danimoral1001@gmail.com>
    Change-Id: I88bc3279552beaf4b68f4ea5d2ee2b9fd52eb0fc

--- packages/SystemUI/src/com/android/systemui/qs/QSFragment.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSFragment.java
@@ -294,6 +294,7 @@ public class QSFragment extends LifecycleFragment implements QS, CommandQueue.Ca
                 : View.INVISIBLE);
         mFooter.setExpanded((keyguardShowing && !mHeaderAnimating && !mShowCollapsedOnKeyguard)
                 || (mQsExpanded && !mStackScrollerOverscrolling));
+		mQSPanel.updateResources();
         mQSPanel.setVisibility(!mQsDisabled && expandVisually ? View.VISIBLE : View.INVISIBLE);
     }
 
