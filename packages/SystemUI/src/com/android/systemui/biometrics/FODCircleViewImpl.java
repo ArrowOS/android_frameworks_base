4db8dfd9469be9342ce76e1a62356112ab9184ff
daniml3 <danimoral1001@gmail.com>
2021-02-27 15:58:25 +0100

    SystemUI: ensure FOD view is correctly hidden
    * This solves some cases where the FOD view has hidden but due to a double tap on the view or some other reason, it was shown again in moments when it shouldn't
    * Solve this by re-hiding the view with some delay after initial hide
    
    Signed-off-by: daniml3 <danimoral1001@gmail.com>
    Change-Id: I76fc92ec37ea13b293af5e2f6941ccff37422713

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleViewImpl.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleViewImpl.java
@@ -18,6 +18,8 @@ package com.android.systemui.biometrics;
 
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Slog;
 import android.view.View;
 
@@ -36,11 +38,18 @@ public class FODCircleViewImpl extends SystemUI implements CommandQueue.Callback
 
     private FODCircleView mFodCircleView;
     private final CommandQueue mCommandQueue;
+    private Handler mHandler;
+    private Runnable mHideFodViewRunnable;
 
     @Inject
     public FODCircleViewImpl(Context context, CommandQueue commandQueue) {
         super(context);
         mCommandQueue = commandQueue;
+        mHandler = new Handler(Looper.getMainLooper());
+
+        mHideFodViewRunnable = () -> {
+            mFodCircleView.hide();
+        };
     }
 
     @Override
@@ -61,6 +70,7 @@ public class FODCircleViewImpl extends SystemUI implements CommandQueue.Callback
     @Override
     public void showInDisplayFingerprintView() {
         if (mFodCircleView != null) {
+            mHandler.removeCallbacks(mHideFodViewRunnable);
             mFodCircleView.show();
         }
     }
@@ -69,6 +79,7 @@ public class FODCircleViewImpl extends SystemUI implements CommandQueue.Callback
     public void hideInDisplayFingerprintView() {
         if (mFodCircleView != null) {
             mFodCircleView.hide();
+            mHandler.postDelayed(mHideFodViewRunnable, 500);
         }
     }
 }
