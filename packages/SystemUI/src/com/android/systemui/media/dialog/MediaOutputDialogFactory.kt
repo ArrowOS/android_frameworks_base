dedf7ad40c155dfe8744bab3c3742e9e14d5aaf0
daniml3 <danimoral1001@gmail.com>
2021-03-06 21:55:08 +0100

    SystemUI: MediaOutputDialogFactory: accept null package name
    * If null, set to an empty string
    
    Signed-off-by: daniml3 <danimoral1001@gmail.com>
    Change-Id: I084a97eb65af80635370b6ab4336950ba350cd2b

--- packages/SystemUI/src/com/android/systemui/media/dialog/MediaOutputDialogFactory.kt
+++ packages/SystemUI/src/com/android/systemui/media/dialog/MediaOutputDialogFactory.kt
@@ -44,7 +44,7 @@ class MediaOutputDialogFactory @Inject constructor(
     }
 
     /** Creates a [MediaOutputDialog] for the given package. */
-    fun create(packageName: String, aboveStatusBar: Boolean) {
+    fun create(packageName: String?, aboveStatusBar: Boolean) {
         mediaOutputDialog?.dismiss()
         mediaOutputDialog = MediaOutputController(context, packageName, aboveStatusBar,
                 mediaSessionManager, lbm, shadeController, starter, notificationEntryManager,
