0ac6eac02a46b7282818964803502ff97ee2e66b
palaych <palaychm@yandex.ru>
2021-07-13 08:19:19 +0900

    base: Remove LockscreenCharging info [1/3]
     * On new QCOM devices reports wrong values, as a result, this data shown on the lock screen is not correct. Better remove this so as not to mislead users.
    
     * It include revert commits:
    
        Revert "LockscreenCharging: squashed (1/3)"
    
        This reverts commit dcd5d5f234677ef070360e9d6f1b007535a60fb3.
    
        Change-Id: I10f2573b9b5b01b99d7c02b194a4b659de801515
    
        Revert "Lockscreen charging info: show decimal in battery temperature"
    
        This reverts commit f2e0f69fcf9800ec24171be95cbd7035025a0ac1.
    
        Change-Id: I8d8d9434fa5ff85d2337c6a3ac48f9e6b5f17e22
    
        Revert "SystemUI: keyguard show charging watt"
    
        This reverts commit c4000751c542725e1ba28b3e11bf9d3fc1ea6d5f.
    
        Revert "SystemUI: Fix FC on lockscreen charging info"
    
        This reverts commit fb4260448539b717106b08a12e75d94113d258f5.
    
        Revert "SystemUI: Fix current formatting on lockscreen"
    
        This reverts commit 3625d993144aee2485058cb0b4ef42b11886006a.
    
        Revert "LockscreenCharging: Calculate power from voltage and current"
    
        This reverts commit 491f6ef7d18488984cc74d0b156e7e80c079f7da.
    
    Change-Id: I4dfeb41bd3d51b13d28a7ede8092787db04492ae

--- packages/SettingsLib/src/com/android/settingslib/fuelgauge/BatteryStatus.java
+++ packages/SettingsLib/src/com/android/settingslib/fuelgauge/BatteryStatus.java
@@ -24,7 +24,6 @@ import static android.os.BatteryManager.EXTRA_HEALTH;
 import static android.os.BatteryManager.EXTRA_LEVEL;
 import static android.os.BatteryManager.EXTRA_MAX_CHARGING_CURRENT;
 import static android.os.BatteryManager.EXTRA_MAX_CHARGING_VOLTAGE;
-import static android.os.BatteryManager.EXTRA_TEMPERATURE;
 import static android.os.BatteryManager.EXTRA_PLUGGED;
 import static android.os.BatteryManager.EXTRA_PRESENT;
 import static android.os.BatteryManager.EXTRA_STATUS;
@@ -53,24 +52,17 @@ public class BatteryStatus {
     public final int level;
     public final int plugged;
     public final int health;
-    public final int maxChargingCurrent;
-    public final int maxChargingVoltage;
     public final int maxChargingWattage;
-    public final float temperature;
     public final boolean dashChargeStatus;
     public final boolean present;
 
     public BatteryStatus(int status, int level, int plugged, int health,
-            int maxChargingCurrent, int maxChargingVoltage,
-            int maxChargingWattage, float temperature, boolean dashChargeStatus, boolean present) {
+            int maxChargingWattage, boolean dashChargeStatus, boolean present) {
         this.status = status;
         this.level = level;
         this.plugged = plugged;
         this.health = health;
-        this.maxChargingCurrent = maxChargingCurrent;
-        this.maxChargingVoltage = maxChargingVoltage;
         this.maxChargingWattage = maxChargingWattage;
-        this.temperature = temperature;
         this.dashChargeStatus = dashChargeStatus;
         this.present = present;
     }
@@ -80,7 +72,6 @@ public class BatteryStatus {
         plugged = batteryChangedIntent.getIntExtra(EXTRA_PLUGGED, 0);
         level = batteryChangedIntent.getIntExtra(EXTRA_LEVEL, 0);
         health = batteryChangedIntent.getIntExtra(EXTRA_HEALTH, BATTERY_HEALTH_UNKNOWN);
-        temperature = batteryChangedIntent.getIntExtra(EXTRA_TEMPERATURE, -1);
         dashChargeStatus = batteryChangedIntent.getBooleanExtra(EXTRA_DASH_CHARGER, false);
         present = batteryChangedIntent.getBooleanExtra(EXTRA_PRESENT, true);
 
@@ -96,12 +87,8 @@ public class BatteryStatus {
             // to maintain precision equally on both factors.
             maxChargingWattage = (maxChargingMicroAmp / 1000)
                     * (maxChargingMicroVolt / 1000);
-            maxChargingCurrent = maxChargingMicroAmp;
-            maxChargingVoltage = maxChargingMicroVolt;
         } else {
             maxChargingWattage = -1;
-            maxChargingCurrent = -1;
-            maxChargingVoltage = -1;
         }
     }
 
