4fd1792066ad4cc3392f319548caca97a1ef218f
palaych <palaychm@yandex.ru>
2021-07-13 08:19:19 +0900

    base: Remove LockscreenCharging info [1/2]
     * On new QCOM devices reports wrong values, as a result, this data shown on the lock screen is not correct. Better remove this so as not to mislead users.
    
     * It include revert commits:
    
        Revert "LockscreenCharging: squashed (1/3)"
    
        This reverts commit dcd5d5f234677ef070360e9d6f1b007535a60fb3.
    
        Change-Id: I10f2573b9b5b01b99d7c02b194a4b659de801515
    
        Revert "Lockscreen charging info: show decimal in battery temperature"
    
        This reverts commit f2e0f69fcf9800ec24171be95cbd7035025a0ac1.
    
        Change-Id: I8d8d9434fa5ff85d2337c6a3ac48f9e6b5f17e22
    
        Revert "SystemUI: keyguard show charging watt"
    
        This reverts commit c4000751c542725e1ba28b3e11bf9d3fc1ea6d5f.
    
        Revert "SystemUI: Fix FC on lockscreen charging info"
    
        This reverts commit fb4260448539b717106b08a12e75d94113d258f5.
    
        Revert "SystemUI: Fix current formatting on lockscreen"
    
        This reverts commit 3625d993144aee2485058cb0b4ef42b11886006a.
    
        Revert "LockscreenCharging: Calculate power from voltage and current"
    
        This reverts commit 491f6ef7d18488984cc74d0b156e7e80c079f7da.
    
    Change-Id: I4dfeb41bd3d51b13d28a7ede8092787db04492ae

--- packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
@@ -1735,7 +1735,7 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
         }
 
         // Take a guess at initial SIM state, battery status and PLMN until we get an update
-        mBatteryStatus = new BatteryStatus(BATTERY_STATUS_UNKNOWN, 100, 0, 0, 0, 0, 0, 0, false, true);
+        mBatteryStatus = new BatteryStatus(BATTERY_STATUS_UNKNOWN, 100, 0, 0, 0, false, true);
 
         // Watch for interesting updates
         final IntentFilter filter = new IntentFilter();
@@ -2646,16 +2646,8 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
             return true;
         }
 
-        // change in battery temperature
-        if (old.temperature != current.temperature) {
-            return true;
-        }
-
         // change in charging current while plugged in
-        if (nowPluggedIn &&
-              (current.maxChargingWattage != old.maxChargingWattage ||
-               current.maxChargingCurrent != old.maxChargingCurrent ||
-               current.maxChargingVoltage != old.maxChargingVoltage)) {
+        if (nowPluggedIn && current.maxChargingWattage != old.maxChargingWattage) {
             return true;
         }
 
--- packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -37,7 +37,6 @@ import android.os.Message;
 import android.os.RemoteException;
 import android.os.UserHandle;
 import android.os.UserManager;
-import android.provider.Settings;
 import android.text.TextUtils;
 import android.text.format.Formatter;
 import android.util.Log;
@@ -122,14 +121,11 @@ public class KeyguardIndicationController implements StateListener,
     private boolean mBatteryOverheated;
     private boolean mEnableBatteryDefender;
     private int mChargingSpeed;
-    private double mChargingWattage;
+    private int mChargingWattage;
     private int mBatteryLevel;
     private boolean mBatteryPresent = true;
     private long mChargingTimeRemaining;
     private float mDisclosureMaxAlpha;
-    private int mChargingCurrent;
-    private double mChargingVoltage;
-    private float mTemperature;
     private String mMessageToShowOnScreenOn;
 
     private KeyguardUpdateMonitorCallback mUpdateMonitorCallback;
@@ -610,35 +606,6 @@ public class KeyguardIndicationController implements StateListener,
                     : R.string.keyguard_plugged_in_wireless;
         }
 
-        String batteryInfo = "";
-        int current = 0;
-        double voltage = 0;
-        boolean showbatteryInfo = Settings.System.getIntForUser(mContext.getContentResolver(),
-            Settings.System.LOCKSCREEN_BATTERY_INFO, 1, UserHandle.USER_CURRENT) == 1;
-        if (showbatteryInfo) {
-            if (mChargingCurrent > 0) {
-                current = (mChargingCurrent < 5 ? (mChargingCurrent * 1000)
-                        : (mChargingCurrent < 4000 ? mChargingCurrent : (mChargingCurrent / 1000)));
-                batteryInfo = batteryInfo + current + "mA";
-            }
-            if (mChargingVoltage > 0 && mChargingCurrent > 0) {
-                voltage = mChargingVoltage / 1000 / 1000;
-                batteryInfo = (batteryInfo == "" ? "" : batteryInfo + " · ") +
-                        String.format("%.1f" , ((double) current / 1000) * voltage) + "W";
-            }
-            if (mChargingVoltage > 0) {
-                batteryInfo = (batteryInfo == "" ? "" : batteryInfo + " · ") +
-                        String.format("%.1f" , voltage) + "V";
-            }
-            if (mTemperature > 0) {
-                batteryInfo = (batteryInfo == "" ? "" : batteryInfo + " · ") +
-                        mTemperature / 10 + "°C";
-            }
-            if (batteryInfo != "") {
-                batteryInfo = "\n" + batteryInfo;
-            }
-        }
-
         if (hasChargingTime) {
             // We now have battery percentage in these strings and it's expected that all
             // locales will also have it in the future. For now, we still have to support the old
@@ -646,21 +613,17 @@ public class KeyguardIndicationController implements StateListener,
             String chargingTimeFormatted = Formatter.formatShortElapsedTimeRoundingUpToMinutes(
                     mContext, mChargingTimeRemaining);
             try {
-                String chargingText = mContext.getResources().getString(chargingId, chargingTimeFormatted,
+                return mContext.getResources().getString(chargingId, chargingTimeFormatted,
                         percentage);
-                return chargingText + batteryInfo;
             } catch (IllegalFormatConversionException e) {
-                String chargingText =  mContext.getResources().getString(chargingId, chargingTimeFormatted);
-                return chargingText + batteryInfo;
+                return mContext.getResources().getString(chargingId, chargingTimeFormatted);
             }
         } else {
             // Same as above
             try {
-                String chargingText =  mContext.getResources().getString(chargingId, percentage);
-                return chargingText + batteryInfo;
+                return mContext.getResources().getString(chargingId, percentage);
             } catch (IllegalFormatConversionException e) {
-                String chargingText =  mContext.getResources().getString(chargingId);
-                return chargingText + batteryInfo;
+                return mContext.getResources().getString(chargingId);
             }
         }
     }
@@ -770,11 +733,8 @@ public class KeyguardIndicationController implements StateListener,
             mPowerPluggedInWired = status.isPluggedInWired() && isChargingOrFull;
             mPowerPluggedIn = status.isPluggedIn() && isChargingOrFull;
             mPowerCharged = status.isCharged();
-            mChargingCurrent = status.maxChargingCurrent;
-            mChargingVoltage = status.maxChargingVoltage;
             mChargingWattage = status.maxChargingWattage;
             mChargingSpeed = status.getChargingSpeed(mContext);
-            mTemperature = status.temperature;
             mBatteryLevel = status.level;
             mBatteryOverheated = status.isOverheated();
             mEnableBatteryDefender = mBatteryOverheated && status.isPluggedIn();
