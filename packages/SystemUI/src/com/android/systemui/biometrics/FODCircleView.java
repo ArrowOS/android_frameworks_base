201c9552e4c7ee3372dcefef4609463b93c12d5c
daniml3 <danimoral1001@gmail.com>
2021-07-19 16:12:35 +0200

    FODCircleView: make the base view's backgrund fully opaque on AOD
    Signed-off-by: daniml3 <danimoral1001@gmail.com>
    Change-Id: Id6646d1807d47c58264b6da17ece27cb8d00e556

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -152,6 +152,13 @@ public class FODCircleView extends ImageView implements TunerService.Tunable {
                 mBurnInProtectionTimer.cancel();
                 updatePosition();
             }
+
+            mParams.dimAmount = dreaming ? 1.0f : 0.0f;
+            if (getParent() == null) {
+                mWindowManager.addView(this, mPressedParams);
+            } else {
+                mWindowManager.updateViewLayout(this, mPressedParams);
+            }
         }
 
         @Override
