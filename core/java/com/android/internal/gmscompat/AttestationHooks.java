5902cee7cd95b8146cc6e64484179f4fddf1615e
Danny Lin <danny@kdrag0n.dev>
2021-10-11 19:59:51 -0700

    Alter model name to avoid SafetyNet HW attestation enforcement
    As of September 2, Google is enforcing SafetyNet's previously
    opportunistic hardware-backed attestation based on device information.
    Append a space to the device model name in order to avoid such
    enforcement.
    
    Also contains:
    	Spoof build fingerprint for Google Play Services
    
    	SafetyNet's CTS profile attestation checks whether Build.FINGERPRINT
    	matches that of the device's stock OS, which has passed CTS testing.
    	Spoof the fingerprint for Google Play Services to help pass SafetyNet.
    
    	We used to set the real system build fingerprint to the stock one, but
    	Android relies on each build having a unique fingerprint in order to
    	clear the correct caches and update persistent state for system changes.
    	On devices that no longer receive updates from the OEM, the build
    	fingerprint never changes and Android doesn't account for updates
    	correctly, which causes issues when updating without wiping data.
    	Only spoofing the fingerprint for Google Play Services fixes this issue.
    
    	Corresponding vendor commit:
    	"Only use stock build fingerprint for Google Play Services"
    
    	NB: This code is under the gmscompat package, but it does not depend on
    	any code from gmscompat.
    
    	Change-Id: I26a2498eb2e2163933303b03f6d516e5fb30fe51
    
    * We don't need to spoof the fingerprint here since we do it globally, but we
      use the Build field spoofing code it added for model
    
    Change-Id: I591960aff76b586405dda94783a9e46d4bf8fca0

--- core/java/com/android/internal/gmscompat/AttestationHooks.java
+++ core/java/com/android/internal/gmscompat/AttestationHooks.java
@@ -17,59 +17,19 @@
 package com.android.internal.gmscompat;
 
 import android.app.Application;
-import android.content.res.Resources;
 import android.os.Build;
 import android.os.SystemProperties;
 import android.util.Log;
 
-import com.android.internal.R;
-
 import java.lang.reflect.Field;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
 /** @hide */
 public final class AttestationHooks {
     private static final String TAG = "GmsCompat/Attestation";
-
     private static final String PACKAGE_GMS = "com.google.android.gms";
-    private static final String PACKAGE_GPHOTOS = "com.google.android.apps.photos";
-
-    private static final String PROCESS_UNSTABLE = "com.google.android.gms.unstable";
-
-    private static final String PRODUCT_GMS_SPOOFING_FINGERPRINT =
-            SystemProperties.get("ro.build.gms_fingerprint");
-
-    private static final Map<String, String> sP1Props = new HashMap<>();
-    static {
-        sP1Props.put("BRAND", "google");
-        sP1Props.put("MANUFACTURER", "Google");
-        sP1Props.put("DEVICE", "marlin");
-        sP1Props.put("PRODUCT", "marlin");
-        sP1Props.put("MODEL", "Pixel XL");
-        sP1Props.put("FINGERPRINT", "google/marlin/marlin:10/QP1A.191005.007.A3/5972272:user/release-keys");
-    }
-
-    private static final String[] sFeaturesBlacklist = {
-        "PIXEL_2017_EXPERIENCE",
-        "PIXEL_2017_PRELOAD",
-        "PIXEL_2018_PRELOAD",
-        "PIXEL_2019_EXPERIENCE",
-        "PIXEL_2019_MIDYEAR_EXPERIENCE",
-        "PIXEL_2019_MIDYEAR_PRELOAD",
-        "PIXEL_2019_PRELOAD",
-        "PIXEL_2020_EXPERIENCE",
-        "PIXEL_2020_MIDYEAR_EXPERIENCE",
-        "PIXEL_2021_EXPERIENCE",
-        "PIXEL_2021_MIDYEAR_EXPERIENCE"
-    };
 
     private static volatile boolean sIsGms = false;
-    private static volatile boolean sIsPhotos = false;
-
-    private static final boolean sSpoofPhotos =
-            Resources.getSystem().getBoolean(R.bool.config_spoofGooglePhotos);
 
     private AttestationHooks() { }
 
@@ -90,32 +50,15 @@ public final class AttestationHooks {
     }
 
     private static void spoofBuildGms() {
-        // Set fingerprint for SafetyNet CTS profile
-        if (PRODUCT_GMS_SPOOFING_FINGERPRINT.length() > 0) {
-            setBuildField("FINGERPRINT", PRODUCT_GMS_SPOOFING_FINGERPRINT);
-        }
-
         // Alter model name to avoid hardware attestation enforcement
         setBuildField("MODEL", Build.MODEL + " ");
     }
 
     public static void initApplicationBeforeOnCreate(Application app) {
-        if (PACKAGE_GMS.equals(app.getPackageName()) &&
-                PROCESS_UNSTABLE.equals(Application.getProcessName())) {
+        if (PACKAGE_GMS.equals(app.getPackageName())) {
             sIsGms = true;
             spoofBuildGms();
-        } else if (sSpoofPhotos && PACKAGE_GPHOTOS.equals(app.getPackageName())) {
-            sIsPhotos = true;
-            sP1Props.forEach((k, v) -> setBuildField(k, v));
         }
-    }
-
-    public static boolean hasSystemFeature(String name, boolean def) {
-        if (sIsPhotos && def &&
-                Arrays.stream(sFeaturesBlacklist).anyMatch(name::contains)) {
-            return false;
-        }
-        return def;
     }
 
     private static boolean isCallerSafetyNet() {
