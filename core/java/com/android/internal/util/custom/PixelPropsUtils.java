29ab0bddf11cd0c8fd8c65bce93a5568465f6994
Ratoriku <a1063021545@gmail.com>
2021-11-03 18:02:03 +0800

    base: PixelPropsUtils: Switch to november raven props
    Signed-off-by: Ratoriku <a1063021545@gmail.com>
    Change-Id: I236617f984f3898a694db0c62224523f80f55c12

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -46,10 +46,10 @@ public class PixelPropsUtils {
         propsToChange = new HashMap<>();
         propsToChange.put("BRAND", "google");
         propsToChange.put("MANUFACTURER", "Google");
-        propsToChange.put("DEVICE", "redfin");
-        propsToChange.put("PRODUCT", "redfin");
-        propsToChange.put("MODEL", "Pixel 5");
-        propsToChange.put("FINGERPRINT", "google/redfin/redfin:12/SP1A.210812.015/7679548:user/release-keys");
+        propsToChange.put("DEVICE", "raven");
+        propsToChange.put("PRODUCT", "raven");
+        propsToChange.put("MODEL", "Pixel 6 Pro");
+        propsToChange.put("FINGERPRINT", "google/raven/raven:12/SD1A.210817.036/7805805:user/release-keys");
     }
 
     public static void setProps(String packageName) {
