32930cc54f696c0975290784bba4386efabb8c23
kubersharma001 <kubersharma001@gmail.com>
2021-12-16 07:16:56 +0000

    base: fixup! "Merge tag 'android-12.0.0_r19'"
    Change-Id: Ia03e8d68a584a642d814a80f2839a8cabfe8d40a

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
@@ -977,9 +977,10 @@ public class NotificationPanelViewController extends PanelViewController {
         mScreenCornerRadius = (int) ScreenDecorationsUtils.getWindowCornerRadius(mResources);
         mLockscreenNotificationQSPadding = mResources.getDimensionPixelSize(
                 R.dimen.notification_side_paddings);
-        mUdfpsMaxYBurnInOffset = mResources.getDimensionPixelSize(R.dimen.udfps_burn_in_offset_y);
         mStatusBarHeaderHeight = mResources.getDimensionPixelSize(
                 R.dimen.status_bar_height);
+
+        mUdfpsMaxYBurnInOffset = mResources.getDimensionPixelSize(R.dimen.udfps_burn_in_offset_y);
     }
 
     private void updateViewControllers(KeyguardStatusView keyguardStatusView,
