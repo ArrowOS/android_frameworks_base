db5118de54ad0907975f517663ff2f5bbd679440
kubersharma001 <kubersharma001@gmail.com>
2021-12-16 07:16:56 +0000

    base: fixup! "Merge tag 'android-12.0.0_r19'"
    Change-Id: Ia03e8d68a584a642d814a80f2839a8cabfe8d40a

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
@@ -317,7 +317,7 @@ public class UdfpsController implements DozeReceiver, UdfpsHbmProvider {
             mFgExecutor.execute(() -> {
                 if (acquiredInfo == 6 && (mStatusBarStateController.isDozing() || !mScreenOn)) {
                     if (vendorCode == mUdfpsVendorCode) {
-                        mPowerManager.wakeUp(SystemClock.uptimeMillis(),
+                        mPowerManager.wakeUp(mSystemClock.uptimeMillis(),
                                 PowerManager.WAKE_REASON_GESTURE, TAG);
                         onAodInterrupt(0, 0, 0, 0); // To-Do pass proper values
                     }
@@ -1051,7 +1051,7 @@ public class UdfpsController implements DozeReceiver, UdfpsHbmProvider {
             @Nullable Runnable onHbmEnabled) {
         // TO-DO send call to lineage biometric hal and/or add dummy jni that device could override
         if (onHbmEnabled != null) {
-            mMainHandler.post(onHbmEnabled);
+            onHbmEnabled.run();
         }
     }
 
@@ -1059,7 +1059,7 @@ public class UdfpsController implements DozeReceiver, UdfpsHbmProvider {
     public void disableHbm(@Nullable Runnable onHbmDisabled) {
         // TO-DO send call to lineage biometric hal and/or add dummy jni that device could override
         if (onHbmDisabled != null) {
-            mMainHandler.post(onHbmDisabled);
+            onHbmDisabled.run();
         }
     }
 }
