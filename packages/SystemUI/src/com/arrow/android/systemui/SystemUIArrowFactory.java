18b82f8a1d65cea88b4a35822b0f7a4b91bab19e
Anay Wadhera <awadhera@berkeley.edu>
2021-12-28 08:31:51 +0530

    SystemUI: integrate smartspace and backgesture provider
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Change-Id: If46fe528418daca5bcd7ce2cde221c9b185947d6

--- packages/SystemUI/src/com/arrow/android/systemui/SystemUIArrowFactory.java
+++ packages/SystemUI/src/com/arrow/android/systemui/SystemUIArrowFactory.java
@@ -1,12 +1,22 @@
 package com.arrow.android.systemui;
 
 import android.content.Context;
+import android.content.res.AssetManager;
+import android.os.Handler;
+
+import com.google.android.systemui.gesture.BackGestureTfClassifierProviderGoogle;
 
 import com.arrow.android.systemui.dagger.DaggerGlobalRootComponentArrow;
 import com.arrow.android.systemui.dagger.GlobalRootComponentArrow;
+import com.arrow.android.systemui.dagger.SysUIComponentArrow;
 
 import com.android.systemui.SystemUIFactory;
 import com.android.systemui.dagger.GlobalRootComponent;
+import com.android.systemui.navigationbar.gestural.BackGestureTfClassifierProvider;
+import com.android.systemui.screenshot.ScreenshotNotificationSmartActionsProvider;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
 
 public class SystemUIArrowFactory extends SystemUIFactory {
     @Override
@@ -14,5 +24,18 @@ public class SystemUIArrowFactory extends SystemUIFactory {
         return DaggerGlobalRootComponentArrow.builder()
                 .context(context)
                 .build();
+    }
+
+    @Override
+    public BackGestureTfClassifierProvider createBackGestureTfClassifierProvider(AssetManager am, String modelName) {
+        return new BackGestureTfClassifierProviderGoogle(am, modelName);
+    }
+
+    @Override
+    public void init(Context context, boolean fromTest) throws ExecutionException, InterruptedException {
+        super.init(context, fromTest);
+        if (shouldInitializeComponents()) {
+            ((SysUIComponentArrow) getSysUIComponent()).createKeyguardSmartspaceController();
+        }
     }
 }
