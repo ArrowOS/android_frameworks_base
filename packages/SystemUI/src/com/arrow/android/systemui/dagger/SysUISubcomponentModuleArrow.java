259aab8e454865b389dd4e0639fa6f0c17fde6d9
Anay Wadhera <awadhera@berkeley.edu>
2021-12-28 05:49:21 +0530

    SystemUI: Initial SystemUI stub
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Change-Id: I7adcb852dba99b67a92afc9fcff016a9f3c9f131

---
+++ packages/SystemUI/src/com/arrow/android/systemui/dagger/SysUISubcomponentModuleArrow.java
@@ -0,0 +1,10 @@
+package com.arrow.android.systemui.dagger;
+
+import dagger.Module;
+
+/**
+ * Dagger module for including the WMComponent.
+ */
+@Module(subcomponents = {SysUIComponentArrow.class})
+public abstract class SysUISubcomponentModuleArrow {
+}
