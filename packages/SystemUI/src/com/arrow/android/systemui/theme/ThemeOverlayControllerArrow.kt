c1ecf99df3c5fba64949686ce8f7d542451d3901
Anay Wadhera <awadhera@berkeley.edu>
2021-12-28 05:57:38 +0530

    SystemUI: update monet according to new sysui stub
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Change-Id: I016289b20c684de8937b97f8b4f20f28a9c26aba

--- packages/SystemUI/src/com/arrow/android/systemui/theme/ThemeOverlayControllerArrow.kt
+++ packages/SystemUI/src/com/arrow/android/systemui/theme/ThemeOverlayControllerArrow.kt
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.protonaosp.systemui.theme
+package com.arrow.android.systemui.theme
 
 import android.annotation.ColorInt
 import android.app.WallpaperColors
@@ -55,7 +55,7 @@ import kotlin.math.log10
 import kotlin.math.pow
 
 @SysUISingleton
-class CustomThemeOverlayController @Inject constructor(
+class ThemeOverlayControllerArrow @Inject constructor(
     private val context: Context,
     broadcastDispatcher: BroadcastDispatcher,
     @Background bgHandler: Handler,
@@ -190,7 +190,7 @@ class CustomThemeOverlayController @Inject constructor(
     }
 
     companion object {
-        private const val TAG = "CustomThemeOverlayController"
+        private const val TAG = "ThemeOverlayControllerArrow"
 
         private const val PREF_PREFIX = "monet_engine"
         private const val PREF_CUSTOM_COLOR = "${PREF_PREFIX}_custom_color"
