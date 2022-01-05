ArrowOS/android_frameworks_base

commit 9ebbcad3b57f58e3b8046501f2ad238e4c7ae8b1

SystemUI: AuthController: Fix ripple animation for side fp devices
 * start animation from right side of display and not middle,
   as thats where the powerbutton/fingerprint sensor is located
   on these devices

Change-Id: I186f1a50e8623055ce6b04aded9b854ac30cbe4a

packages/SystemUI/src/com/android/systemui/biometrics/AuthController.java
@@ -456,7 +456,8 @@ public class AuthController extends SystemUI implements CommandQueue.Callbacks,
                     (float) faceAuthLocation[1]);
         }
 
-        mFingerprintLocation = new PointF(DisplayUtils.getWidth(mContext) / 2,
+        mFingerprintLocation = new PointF(
+                DisplayUtils.getWidth(mContext) / (mFingerprintManager.isPowerbuttonFps() ? 1 : 2),
                 mContext.getResources().getDimensionPixelSize(
                 com.android.systemui.R.dimen.physical_fingerprint_sensor_center_screen_location_y));
 

commit 5006211b5c87ac8e3b51ec274da2bc9038406755

SystemUI: AuthController: Fix NPE due to isPowerbuttonFps()
 * in some cases mFingerprintManager is null and crashes systemui
 * add null check for it like in other instances

Fixes: 9ebbcad3b57f58e3b8046501f2ad238e4c7ae8b1 ("SystemUI: AuthController: Fix ripple animation for side fp devices")
Change-Id: I32f7ca94474c96b7304cf10938d0c41b13738754

packages/SystemUI/src/com/android/systemui/biometrics/AuthController.java
@@ -479,8 +479,11 @@ public class AuthController extends SystemUI implements CommandQueue.Callbacks,
                     (float) faceAuthLocation[1]);
         }
 
+        boolean isPowerbuttonFps =
+                mFingerprintManager != null && mFingerprintManager.isPowerbuttonFps();
+
         mFingerprintLocation = new PointF(
-                DisplayUtils.getWidth(mContext) / (mFingerprintManager.isPowerbuttonFps() ? 1 : 2),
+                DisplayUtils.getWidth(mContext) / (isPowerbuttonFps ? 1 : 2),
                 mContext.getResources().getDimensionPixelSize(
                 com.android.systemui.R.dimen.physical_fingerprint_sensor_center_screen_location_y));
 