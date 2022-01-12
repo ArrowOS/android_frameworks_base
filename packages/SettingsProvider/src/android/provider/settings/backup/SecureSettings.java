2f99446cfb156815524dead8022b37382ad1f04b
SagarMakhar <sagarmakhar@gmail.com>
2022-01-12 18:32:54 +0000

    base: SystemUI: Add Black theme [1/4]
    [palaych: Add settings key into backup and validators]
    
    Author: Pranav Vashi <neobuddy89@gmail.com>
    Date:   Sat Apr 16 10:38:36 2022 +0530
    
        Fixes and improvements for Black Theme
    
        * Add to SettingsObserver for observing toggle behavior.
        * Apply only with dark theme - looks awful on light mode.
        * Add configuration listener to reapply theme when dark mode changes.
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Change-Id: Ib165e7ba0288eaad7a1e3c1109638acdb66fa05c
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -206,5 +206,6 @@ public class SecureSettings {
         Settings.Secure.LOCKSCREEN_USE_DOUBLE_LINE_CLOCK,
         Settings.Secure.STATUS_BAR_SHOW_VIBRATE_ICON,
         Settings.Secure.TETHERING_ALLOW_VPN_UPSTREAMS,
+        Settings.Secure.SYSTEM_BLACK_THEME,
     };
 }
