580155356fff058763cbf9ab217394b064c843c9
Pranav Vashi <neobuddy89@gmail.com>
2022-01-17 21:20:04 +0530

    SystemUI: Allow toggling privacy indicators [1/2]
    Co-authored-by: jhonboy121 <alfredmathew05@gmail.com>
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>
    Change-Id: Ib0352f3b21eed2048056b3f93862a9e342997bdf
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/HeaderPrivacyIconsController.kt
+++ packages/SystemUI/src/com/android/systemui/qs/HeaderPrivacyIconsController.kt
@@ -142,9 +142,9 @@ class HeaderPrivacyIconsController @Inject constructor(
                 privacyDialogController.showDialog(privacyChip.context)
             }
         }
-        setChipVisibility(privacyChip.visibility == View.VISIBLE)
         micCameraIndicatorsEnabled = privacyItemController.micCameraAvailable
         locationIndicatorsEnabled = privacyItemController.locationAvailable
+        setChipVisibility(privacyChip.visibility == View.VISIBLE)
 
         // Ignore privacy icons because they show in the space above QQS
         updatePrivacyIconSlots()
@@ -171,6 +171,7 @@ class HeaderPrivacyIconsController @Inject constructor(
     }
 
     fun onParentInvisible() {
+        setChipVisibility(false)
         chipVisibilityListener = null
         privacyChip.setOnClickListener(null)
     }
@@ -187,6 +188,7 @@ class HeaderPrivacyIconsController @Inject constructor(
         listening = false
         privacyItemController.removeCallback(picCallback)
         privacyChipLogged = false
+        setChipVisibility(false)
     }
 
     private fun setChipVisibility(visible: Boolean) {
