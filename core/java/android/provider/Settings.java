4f1cac92f51d7ea3c2d9099ac14b5a6c500f62db
Pranav Vashi <neobuddy89@gmail.com>
2022-01-30 00:14:20 +0530

    SystemUI: Forward port pixel navbar animation from redfin 11 [1/2]
    @neobuddy89:
    * This is stripped down version of animation only from Google Assist port by jhenrique.
    * Adapted for Android 12.
    * History commits and fixes can be viewed here:
      https://github.com/crdroidandroid/android_frameworks_base/commits/11.0/packages/SystemUI/src/com/google/android
    
    Change-Id: Ia1062ec1dcb785a98efbd9b85b00eec22902d704
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5692,6 +5692,12 @@ public final class Settings {
         public static final String FORCE_FULLSCREEN_CUTOUT_APPS = "force_full_screen_cutout_apps";
 
         /**
+         * Whether to enable the pixel navbar animation
+         * @hide
+         */
+        public static final String PIXEL_NAV_ANIMATION = "pixel_nav_animation";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
