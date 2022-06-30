49815307a3d0da53a8aaa79b353fc12d42a95661
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-06-30 16:10:53 +0530

    SystemUI: Fix QS mobile icon disappearing on theme switch
    When there is only single SIM, the signal icon disappears
    from expanded QS header on switching dark theme QS tile.
    Ensure that the signal icon is always shown on single SIM.
    
    Change-Id: I089731fe5815a0b2bcbbed1002ca65aa5d3301cd
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -187,6 +187,9 @@ public class QuickStatusBarHeader extends FrameLayout {
 
     void setIsSingleCarrier(boolean isSingleCarrier) {
         mIsSingleCarrier = isSingleCarrier;
+        if (mIsSingleCarrier) {
+            mIconContainer.removeIgnoredSlots(mRssiIgnoredSlots);
+        }
         updateAlphaAnimator();
     }
 
