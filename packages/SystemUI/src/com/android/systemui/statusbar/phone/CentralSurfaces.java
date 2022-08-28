d7ac3df9d192e0cf54eee0afda3c562a013f5cbf
Genkzsz11 <genkzsz11@gmail.com>
2022-08-28 15:57:28 +0700

    SystemUI: Update gamespace for A13
    Change-Id: I4b2701e0b0c01166f152d00da1d3714108e55a99

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfaces.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfaces.java
@@ -56,6 +56,7 @@ import com.android.systemui.statusbar.GestureRecorder;
 import com.android.systemui.statusbar.LightRevealScrim;
 import com.android.systemui.statusbar.NotificationPresenter;
 import com.android.systemui.statusbar.notification.row.NotificationGutsManager;
+import com.android.systemui.statusbar.policy.GameSpaceManager;
 
 import java.io.PrintWriter;
 
@@ -575,6 +576,8 @@ public interface CentralSurfaces extends Dumpable, ActivityStarter, LifecycleOwn
     float getDisplayDensity();
 
     void extendDozePulse();
+
+    GameSpaceManager getGameSpaceManager();
 
     public static class KeyboardShortcutsMessage {
         final int mDeviceId;
