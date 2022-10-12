984fddd17c132c7af9911ec947c3979799cbb33a
Pranav Vashi <neobuddy89@gmail.com>
2022-10-12 18:58:57 +0530

    Add interface for battery stats reset
    * Required for smart charging
    * Rewrite for A13
    
    Change-Id: Ib96252fded922f88550df0988ce2170f8c7d5174
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/os/BatteryManager.java
+++ core/java/android/os/BatteryManager.java
@@ -408,4 +408,12 @@ public class BatteryManager {
             throw e.rethrowFromSystemServer();
         }
     }
+
+    public void resetStatistics() {
+        try {
+            mBatteryStats.resetStatistics();
+        } catch (RemoteException e) {
+            throw e.rethrowFromSystemServer();
+        }
+    }
 }
