1666c2cfa2e723285244fe04f13f97dc4e1a8daa
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-11-10 01:20:15 +0530

    SystemUI: Make advanced reboot optional
    Allow toggling it, and disable it by default.
    
    Change-Id: If10fbf5dc591858255e399108758a9e5b4ca9cfb

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5546,6 +5546,12 @@ public final class Settings {
         public static final String VIBRATE_ON_DISCONNECT = "vibrate_on_disconnect";
 
         /**
+         * Whether to show advanced reboot options in power menu
+         * @hide
+         */
+        public static final String ADVANCED_REBOOT = "advanced_reboot";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
