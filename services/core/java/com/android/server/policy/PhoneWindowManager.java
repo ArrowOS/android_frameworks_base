82736299e268e5e49e2f5b2484ef4e88fc00261e
Ido Ben-Hur <idoybh2@gmail.com>
2023-03-18 21:10:56 +0200

    PhoneWindowManager: Fix power for torch gesture skips power wake events
    Shortly after going to AOD state could still be interactive
    which will make the power button to always skip waking when the gesture is enabled
    use a global bool and better logic to care for that.
    
    Test: in both LP and off non LP (few first seconds in AOD):
    a single power press should wake the device when the gesture is enabled
    
    Change-Id: I7b7069a9b52333b06d96aab38de6a2b710be9ae0
    Signed-off-by: palaych <palaych@arrowos.net>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -639,6 +639,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
     private final List<DeviceKeyHandler> mDeviceKeyHandlers = new ArrayList<>();
     private int mTorchActionMode;
+    private boolean mUnhandledTorchPower = false;
 
     private static final int MSG_DISPATCH_MEDIA_KEY_WITH_WAKE_LOCK = 3;
     private static final int MSG_DISPATCH_MEDIA_KEY_REPEAT_WITH_WAKE_LOCK = 4;
@@ -956,6 +957,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             if (!interactive) {
                 if (mTorchActionMode == 0) {
                     wakeUpFromPowerKey(event.getDownTime());
+                } else {
+                    mUnhandledTorchPower = true;
                 }
             }
         } else {
@@ -999,6 +1002,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
 
         final boolean interactive = Display.isOnState(mDefaultDisplay.getState());
+        final boolean torchActionEnabled = mTorchActionMode != 0;
 
         Slog.d(TAG, "powerPress: eventTime=" + eventTime + " interactive=" + interactive
                 + " count=" + count + " beganFromNonInteractive=" + beganFromNonInteractive
@@ -1011,6 +1015,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         } else if (count > 3 && count <= getMaxMultiPressPowerCount()) {
             Slog.d(TAG, "No behavior defined for power press count " + count);
         } else if (count == 1 && interactive) {
+            if (mUnhandledTorchPower && beganFromNonInteractive && torchActionEnabled) {
+                wakeUpFromPowerKey(eventTime);
+                return;
+            }
             if (beganFromNonInteractive) {
                 // The screen off case, where we might want to start dreaming on power button press.
                 attemptToDreamFromShortPowerButtonPress(false, () -> {});
@@ -1071,7 +1079,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     break;
                 }
             }
-        } else if (mTorchActionMode != 0 && beganFromNonInteractive) {
+        }
+        if (mUnhandledTorchPower && torchActionEnabled && beganFromNonInteractive) {
             wakeUpFromPowerKey(eventTime);
         }
     }
