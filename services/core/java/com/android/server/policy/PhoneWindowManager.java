76c46522b7da59023520579f37c1513cc56e2974
DtHnAme <1904685725@qq.com>
2023-04-05 20:40:43 +0800

    base: Support for device specific input Filter
    Change-Id: I4cdd284bbd064ead6108bac8b54f6a69a9b655e0

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -169,6 +169,7 @@ import android.view.Display;
 import android.view.HapticFeedbackConstants;
 import android.view.IDisplayFoldListener;
 import android.view.InputDevice;
+import android.view.InputFilter;
 import android.view.KeyCharacterMap;
 import android.view.KeyCharacterMap.FallbackAction;
 import android.view.KeyEvent;
@@ -639,6 +640,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
     private final List<DeviceKeyHandler> mDeviceKeyHandlers = new ArrayList<>();
     private int mTorchActionMode;
+    private InputFilter mInputFilter;
 
     private static final int MSG_DISPATCH_MEDIA_KEY_WITH_WAKE_LOCK = 3;
     private static final int MSG_DISPATCH_MEDIA_KEY_REPEAT_WITH_WAKE_LOCK = 4;
@@ -2233,6 +2235,20 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
         if (DEBUG_INPUT) {
             Slog.d(TAG, "" + mDeviceKeyHandlers.size() + " device key handlers loaded");
+        }
+
+        final String deviceInputFilterLibs = res.getString(R.string.config_deviceInputFilterLibs);
+        final String deviceInputFilterClasses = res.getString(R.string.config_deviceInputFilterClasses);
+
+        try {
+            PathClassLoader loader = new PathClassLoader(deviceInputFilterLibs, getClass().getClassLoader());
+            Class<?> klass = loader.loadClass(deviceInputFilterClasses);
+            Constructor<?> constructor = klass.getConstructor(Context.class);
+            mInputFilter = (InputFilter) constructor.newInstance(mContext);
+        } catch (Exception e) {
+            Slog.w(TAG, "Could not instantiate device input filter "
+                    + deviceInputFilterLibs + " from class "
+                    + deviceInputFilterClasses, e);
         }
     }
 
@@ -5308,6 +5324,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
         mAutofillManagerInternal = LocalServices.getService(AutofillManagerInternal.class);
         mGestureLauncherService = LocalServices.getService(GestureLauncherService.class);
+
+        if (mInputFilter != null) {
+            mWindowManagerInternal.setInputFilter(mInputFilter);
+        }
     }
 
     /** {@inheritDoc} */
