00358ac1dd0e0957974a5bc1262f921c5e68aa13
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-04-21 23:39:51 +0530

    PIHooks: Spoof Snapchat to Pixel XL
    Inspired by
    https://github.com/PixysOS/frameworks_base/commit/3afdf68b5bb48a89eb8b3782cca0fa354ab9889f
    https://github.com/PixysOS/frameworks_base/commit/b0475ea8f522d3a156ed262189bc4c70bf91b24f
    
    Co-authored-by: aswin7469 <aswinas@pixysos.com>
    Co-authored-by: John Galt <johngaltfirstrun@gmail.com>
    Change-Id: I8b5d7a6a59b0e049bad07b69d9df847890ba5881

--- core/java/com/android/internal/util/PropImitationHooks.java
+++ core/java/com/android/internal/util/PropImitationHooks.java
@@ -48,6 +48,7 @@ public class PropImitationHooks {
     private static final String PACKAGE_GMS = "com.google.android.gms";
     private static final String PROCESS_GMS_UNSTABLE = PACKAGE_GMS + ".unstable";
     private static final String PACKAGE_GPHOTOS = "com.google.android.apps.photos";
+    private static final String PACKAGE_SNAPCHAT = "com.snapchat.android";
 
     private static final Map<String, Object> sPixelXLProps = Map.of(
         "BRAND", "google",
@@ -86,8 +87,9 @@ public class PropImitationHooks {
         } else if (!sStockFp.isEmpty() && packageName.equals(PACKAGE_ARCORE)) {
             dlog("Setting stock fingerprint for: " + packageName);
             setPropValue("FINGERPRINT", sStockFp);
-        } else if (sSpoofPhotos && packageName.equals(PACKAGE_GPHOTOS)) {
-            dlog("Spoofing Pixel XL for Google Photos");
+        } else if ((sSpoofPhotos && packageName.equals(PACKAGE_GPHOTOS))
+                || packageName.equals(PACKAGE_SNAPCHAT)) {
+            dlog("Spoofing Pixel XL for: " + packageName);
             sPixelXLProps.forEach((PropImitationHooks::setPropValue));
         }
     }
