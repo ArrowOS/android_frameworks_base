ArrowOS/android_frameworks_base

commit 1b894eb5f6e4a534e9df0e5b9a48976086e5fa17

SystemUI: Introduce HD & wifi calling statusbar icons
HD icon shows for VoLTE/VoNR, and wifi calling for VoWiFi.
Based on CLO's volte/vowifi icon implementation, reworked manually.
Drawables taken from Nothing OS and manually tweaked.

Co-authored-by: Weijie Wang <quic_weijiew@quicinc.com>
Change-Id: I0190394690e9b38e0575ffb099be8a5ed2ae9d90

packages/SystemUI/res-keyguard/layout/status_bar_mobile_signal_group_inner.xml
@@ -66,9 +66,9 @@
             android:scaleType="centerInside"
             android:visibility="gone" />
         <Space
-            android:id="@+id/mobile_roaming_space"
+            android:id="@+id/mobile_hd_space"
             android:layout_height="match_parent"
-            android:layout_width="@dimen/roaming_icon_start_padding"
+            android:layout_width="2.5dp"
             android:visibility="gone"
             />
         <FrameLayout
@@ -82,11 +82,10 @@
                 systemui:hasOverlappingRendering="false"
                 />
             <ImageView
-                android:id="@+id/mobile_roaming"
+                android:id="@+id/mobile_hd"
                 android:layout_width="wrap_content"
                 android:layout_height="wrap_content"
-                android:src="@drawable/stat_sys_roaming"
-                android:contentDescription="@string/data_connection_roaming"
+                android:src="@drawable/ic_hd_calling"
                 android:visibility="gone" />
         </FrameLayout>
         <ImageView

packages/SystemUI/res/drawable/ic_hd_calling.xml
@@ -1,0 +1,13 @@
+<vector
+    xmlns:android="http://schemas.android.com/apk/res/android"
+    android:width="@dimen/signal_icon_size"
+    android:height="@dimen/signal_icon_size"
+    android:viewportWidth="24"
+    android:viewportHeight="24">
+    <path
+        android:pathData="M 5.4024 8.2 L 4.2252 8.2 L 4.2252 5.0008 L 1.182 5.0008 L 1.182 8.2 L 0 8.2 L 0 1 L 1.182 1 L 1.182 3.9964 L 4.2252 3.9964 L 4.2252 1 L 5.4024 1 L 5.4024 8.2 Z"
+        android:fillColor="#000000"/>
+    <path
+        android:pathData="M 6.924 8.2 L 6.924 1 L 8.934 1 C 9.535 1 10.068 1.142 10.532 1.425 C 10.999 1.709 11.362 2.111 11.617 2.632 C 11.872 3.153 12 3.749 12 4.422 L 12 4.782 C 12 5.465 11.87 6.065 11.612 6.582 C 11.357 7.101 10.991 7.499 10.513 7.78 C 10.04 8.06 9.497 8.2 8.882 8.2 L 6.924 8.2 Z M 8.107 2.009 L 8.107 7.202 L 8.878 7.202 C 9.498 7.202 9.973 6.996 10.303 6.588 C 10.637 6.176 10.807 5.585 10.813 4.817 L 10.813 4.418 C 10.813 3.635 10.652 3.039 10.332 2.627 C 10.01 2.214 9.545 2.009 8.934 2.009 L 8.107 2.009 Z"
+        android:fillColor="#000000"/>
+</vector>

packages/SystemUI/src/com/android/systemui/statusbar/StatusBarMobileView.java
@@ -59,8 +59,8 @@ public class StatusBarMobileView extends BaseStatusBarFrameLayout implements Dar
     private View mInoutContainer;
     private ImageView mIn;
     private ImageView mOut;
-    private ImageView mMobile, mMobileType, mMobileRoaming;
-    private View mMobileTypeSpace, mMobileRoamingSpace;
+    private ImageView mMobile, mMobileType, mMobileHd;
+    private View mMobileTypeSpace, mMobileHdSpace;
     @StatusBarIconView.VisibleState
     private int mVisibleState = STATE_HIDDEN;
     private DualToneHandler mDualToneHandler;
@@ -125,8 +125,8 @@ public class StatusBarMobileView extends BaseStatusBarFrameLayout implements Dar
         mMobileGroup = findViewById(R.id.mobile_group);
         mMobile = findViewById(R.id.mobile_signal);
         mMobileType = findViewById(R.id.mobile_type);
-        mMobileRoaming = findViewById(R.id.mobile_roaming);
-        mMobileRoamingSpace = findViewById(R.id.mobile_roaming_space);
+        mMobileHd = findViewById(R.id.mobile_hd);
+        mMobileHdSpace = findViewById(R.id.mobile_hd_space);
         mMobileTypeSpace = findViewById(R.id.mobile_type_space);
         mIn = findViewById(R.id.mobile_in);
         mOut = findViewById(R.id.mobile_out);
@@ -186,8 +186,8 @@ public class StatusBarMobileView extends BaseStatusBarFrameLayout implements Dar
         }
         mMobileTypeSpace.setVisibility(mState.typeSpacerVisible ? View.VISIBLE : View.GONE);
         mMobile.setVisibility(mState.showTriangle ? View.VISIBLE : View.GONE);
-        mMobileRoaming.setVisibility(mState.roaming ? View.VISIBLE : View.GONE);
-        mMobileRoamingSpace.setVisibility(mState.roaming ? View.VISIBLE : View.GONE);
+        mMobileHd.setVisibility(mState.showHd ? View.VISIBLE : View.GONE);
+        mMobileHdSpace.setVisibility(mState.showHd ? View.VISIBLE : View.GONE);
         mIn.setVisibility(mState.activityIn ? View.VISIBLE : View.GONE);
         mOut.setVisibility(mState.activityOut ? View.VISIBLE : View.GONE);
         mInoutContainer.setVisibility((mState.activityIn || mState.activityOut)
@@ -220,14 +220,14 @@ public class StatusBarMobileView extends BaseStatusBarFrameLayout implements Dar
         }
         mMobileTypeSpace.setVisibility(state.typeSpacerVisible ? View.VISIBLE : View.GONE);
         mMobile.setVisibility(state.showTriangle ? View.VISIBLE : View.GONE);
-        mMobileRoaming.setVisibility(state.roaming ? View.VISIBLE : View.GONE);
-        mMobileRoamingSpace.setVisibility(state.roaming ? View.VISIBLE : View.GONE);
+        mMobileHd.setVisibility(state.showHd ? View.VISIBLE : View.GONE);
+        mMobileHdSpace.setVisibility(state.showHd ? View.VISIBLE : View.GONE);
         mIn.setVisibility(state.activityIn ? View.VISIBLE : View.GONE);
         mOut.setVisibility(state.activityOut ? View.VISIBLE : View.GONE);
         mInoutContainer.setVisibility((state.activityIn || state.activityOut)
                 ? View.VISIBLE : View.GONE);
 
-        needsLayout |= state.roaming != mState.roaming
+        needsLayout |= state.showHd != mState.showHd
                 || state.activityIn != mState.activityIn
                 || state.activityOut != mState.activityOut
                 || state.showTriangle != mState.showTriangle;
@@ -245,7 +245,7 @@ public class StatusBarMobileView extends BaseStatusBarFrameLayout implements Dar
         mIn.setImageTintList(color);
         mOut.setImageTintList(color);
         mMobileType.setImageTintList(color);
-        mMobileRoaming.setImageTintList(color);
+        mMobileHd.setImageTintList(color);
         mDotView.setDecorColor(tint);
         mDotView.setIconColor(tint, false);
     }
@@ -266,7 +266,7 @@ public class StatusBarMobileView extends BaseStatusBarFrameLayout implements Dar
         mIn.setImageTintList(list);
         mOut.setImageTintList(list);
         mMobileType.setImageTintList(list);
-        mMobileRoaming.setImageTintList(list);
+        mMobileHd.setImageTintList(list);
         mDotView.setDecorColor(color);
     }
 

packages/SettingsLib/res/values/arrow_config.xml
@@ -1,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!--
+     Copyright (C) 2023 ArrowOS
+     SPDX-License-Identifier: Apache-2.0
+-->
+<resources>
+
+    <!-- Roaming symbol (R) path for SignalDrawable as defined on a 24x24 canvas. -->
+    <string name="config_signalRoamingPath" translatable="false">
+        M 19.436 19.688 L 18.152 19.688 L 18.152 22 L 17 22 L 17 16 L 19.332 16 C 20.097 16 20.687 16.155 21.103 16.466 C 21.519 16.776 21.727 17.225 21.727 17.813 C 21.727 18.214 21.619 18.551 21.403 18.823 C 21.191 19.092 20.893 19.299 20.511 19.445 L 22 21.946 L 22 22 L 20.766 22 L 19.436 19.688 Z M 18.152 18.852 L 19.336 18.852 C 19.725 18.852 20.028 18.764 20.247 18.588 C 20.465 18.409 20.575 18.166 20.575 17.859 C 20.575 17.537 20.473 17.288 20.27 17.113 C 20.069 16.937 19.769 16.846 19.368 16.841 L 18.152 16.841 L 18.152 18.852 Z
+    </string>
+    <!-- config_roamingCutout{Height,Width}Fraction define fraction of the 24x24 canvas that
+         should be cut out to display config_signalRoamingPath. -->
+    <item name="config_roamingCutoutWidthFraction" format="float" type="dimen">8</item>
+    <item name="config_roamingCutoutHeightFraction" format="float" type="dimen">9</item>
+
+</resources>

packages/SettingsLib/src/com/android/settingslib/graph/SignalDrawable.java
@@ -62,7 +62,8 @@ public class SignalDrawable extends DrawableWrapper {
     private static final int STATE_SHIFT = 16;
     private static final int STATE_MASK = 0xff << STATE_SHIFT;
     private static final int STATE_CUT = 2;
-    private static final int STATE_CARRIER_CHANGE = 3;
+    private static final int STATE_CUT_R = 3;
+    private static final int STATE_CARRIER_CHANGE = 4;
 
     private static final long DOT_DELAY = 1000;
 
@@ -73,11 +74,15 @@ public class SignalDrawable extends DrawableWrapper {
     private final Path mCutoutPath = new Path();
     private final Path mForegroundPath = new Path();
     private final Path mAttributionPath = new Path();
+    private final Path mRoamingPath = new Path();
     private final Matrix mAttributionScaleMatrix = new Matrix();
     private final Path mScaledAttributionPath = new Path();
+    private final Path mScaledRoamingPath = new Path();
     private final Handler mHandler;
     private final float mCutoutWidthFraction;
     private final float mCutoutHeightFraction;
+    private final float mRCutoutWidthFraction;
+    private final float mRCutoutHeightFraction;
     private float mDarkIntensity = -1;
     private final int mIntrinsicSize;
     private boolean mAnimating;
@@ -87,12 +92,19 @@ public class SignalDrawable extends DrawableWrapper {
         super(context.getDrawable(com.android.internal.R.drawable.ic_signal_cellular));
         final String attributionPathString = context.getString(
                 com.android.internal.R.string.config_signalAttributionPath);
+        final String roamingPathString = context.getString(
+                R.string.config_signalRoamingPath);
         mAttributionPath.set(PathParser.createPathFromPathData(attributionPathString));
+        mRoamingPath.set(PathParser.createPathFromPathData(roamingPathString));
         updateScaledAttributionPath();
         mCutoutWidthFraction = context.getResources().getFloat(
                 com.android.internal.R.dimen.config_signalCutoutWidthFraction);
         mCutoutHeightFraction = context.getResources().getFloat(
                 com.android.internal.R.dimen.config_signalCutoutHeightFraction);
+        mRCutoutWidthFraction = context.getResources().getFloat(
+                R.dimen.config_roamingCutoutWidthFraction);
+        mRCutoutHeightFraction = context.getResources().getFloat(
+                R.dimen.config_roamingCutoutHeightFraction);
         mDarkModeFillColor = Utils.getColorStateListDefaultColor(context,
                 R.color.dark_mode_icon_color_single_tone);
         mLightModeFillColor = Utils.getColorStateListDefaultColor(context,
@@ -112,6 +124,7 @@ public class SignalDrawable extends DrawableWrapper {
                     getBounds().width() / VIEWPORT, getBounds().height() / VIEWPORT);
         }
         mAttributionPath.transform(mAttributionScaleMatrix, mScaledAttributionPath);
+        mRoamingPath.transform(mAttributionScaleMatrix, mScaledRoamingPath);
     }
 
     @Override
@@ -213,16 +226,20 @@ public class SignalDrawable extends DrawableWrapper {
             drawDotAndPadding(x - dotSpacing * 2, y, dotPadding, dotSize, 0);
             canvas.drawPath(mCutoutPath, mTransparentPaint);
             canvas.drawPath(mForegroundPath, mForegroundPaint);
-        } else if (isInState(STATE_CUT)) {
-            float cutX = (mCutoutWidthFraction * width / VIEWPORT);
-            float cutY = (mCutoutHeightFraction * height / VIEWPORT);
+        } else if (isInState(STATE_CUT) || isInState(STATE_CUT_R)) {
+            boolean isRoaming = isInState(STATE_CUT_R);
+            float cutWidth = isRoaming ? mRCutoutWidthFraction : mCutoutWidthFraction;
+            float cutHeight = isRoaming ? mRCutoutHeightFraction : mCutoutHeightFraction;
+            float cutX = (cutWidth * width / VIEWPORT);
+            float cutY = (cutHeight * height / VIEWPORT);
             mCutoutPath.moveTo(width, height);
             mCutoutPath.rLineTo(-cutX, 0);
             mCutoutPath.rLineTo(0, -cutY);
             mCutoutPath.rLineTo(cutX, 0);
             mCutoutPath.rLineTo(0, cutY);
             canvas.drawPath(mCutoutPath, mTransparentPaint);
-            canvas.drawPath(mScaledAttributionPath, mForegroundPaint);
+            canvas.drawPath(isRoaming ? mScaledRoamingPath : mScaledAttributionPath,
+                    mForegroundPaint);
         }
         if (isRtl) {
             canvas.restore();
@@ -284,10 +301,14 @@ public class SignalDrawable extends DrawableWrapper {
         return (fullState & STATE_MASK) >> STATE_SHIFT;
     }
 
-    public static int getState(int level, int numLevels, boolean cutOut) {
-        return ((cutOut ? STATE_CUT : 0) << STATE_SHIFT)
+    public static int getState(int level, int numLevels, boolean cutOut, boolean roaming) {
+        return ((cutOut ? STATE_CUT : roaming ? STATE_CUT_R : 0) << STATE_SHIFT)
                 | (numLevels << NUM_LEVEL_SHIFT)
                 | level;
+    }
+
+    public static int getState(int level, int numLevels, boolean cutOut) {
+        return getState(level, numLevels, cutOut, false);
     }
 
     /** Returns the state representing empty mobile signal with the given number of levels. */

packages/SystemUI/res/drawable/ic_statusbar_hd_calling.xml
@@ -1,0 +1,17 @@
+<vector
+    xmlns:android="http://schemas.android.com/apk/res/android"
+    android:width="24dp"
+    android:height="24dp"
+    android:viewportWidth="24"
+    android:viewportHeight="24"
+    android:tint="?android:attr/colorControlNormal">
+    <path
+        android:pathData="M 22 2 L 2 22 L 22 22 L 22 2 L 22 2 Z"
+        android:fillColor="#000000"/>
+    <path
+        android:pathData="M 5.402 8.2 L 4.225 8.2 L 4.225 5.001 L 1.182 5.001 L 1.182 8.2 L 0 8.2 L 0 1 L 1.182 1 L 1.182 3.996 L 4.225 3.996 L 4.225 1 L 5.402 1 L 5.402 8.2 Z"
+        android:fillColor="#000000"/>
+    <path
+        android:pathData="M 6.924 8.2 L 6.924 1 L 8.934 1 C 9.535 1 10.068 1.142 10.532 1.425 C 10.999 1.709 11.362 2.111 11.617 2.632 C 11.872 3.153 12 3.749 12 4.422 L 12 4.782 C 12 5.465 11.87 6.065 11.612 6.582 C 11.357 7.101 10.991 7.499 10.513 7.78 C 10.04 8.06 9.497 8.2 8.882 8.2 L 6.924 8.2 Z M 8.107 2.009 L 8.107 7.202 L 8.878 7.202 C 9.498 7.202 9.973 6.996 10.303 6.588 C 10.637 6.176 10.807 5.585 10.813 4.817 L 10.813 4.418 C 10.813 3.635 10.652 3.039 10.332 2.627 C 10.01 2.214 9.545 2.009 8.934 2.009 L 8.107 2.009 Z"
+        android:fillColor="#000000"/>
+</vector>

packages/SystemUI/res/drawable/ic_statusbar_roaming.xml
@@ -13,13 +13,18 @@
      See the License for the specific language governing permissions and
      limitations under the License.
 -->
-<vector xmlns:android="http://schemas.android.com/apk/res/android"
-        android:width="16dp"
-        android:height="16dp"
-        android:viewportWidth="16.0"
-        android:viewportHeight="16.0"
-        android:tint="?android:attr/colorControlNormal">
+<vector
+    xmlns:android="http://schemas.android.com/apk/res/android"
+    android:width="24dp"
+    android:height="24dp"
+    android:viewportWidth="24"
+    android:viewportHeight="24"
+    android:tint="?android:attr/colorControlNormal">
     <path
-        android:fillColor="#FFFFFFFF"
-        android:pathData="M4.5,13.02V3h3.93c0.97,0 1.75,0.27 2.34,0.81c0.6,0.53 0.9,1.28 0.9,2.25c0,0.63 -0.19,1.2 -0.56,1.74c-0.37,0.53 -0.95,0.91 -1.74,1.12l2.45,4.02v0.08h-1.69L7.75,9.09H6.03v3.93H4.5zM8.46,4.44H6.03v3.23h2.44c0.49,0 0.9,-0.14 1.2,-0.41c0.31,-0.28 0.46,-0.69 0.46,-1.22s-0.15,-0.93 -0.45,-1.2C9.38,4.58 8.98,4.44 8.46,4.44z"/>
+        android:pathData="M 22 2 L 2 22 L 22 22 L 22 2 L 22 2 Z M 16 15 L 22 15 L 22 22 L 16 22 L 16 15"
+        android:fillColor="#000000"
+        android:fillType="evenOdd"/>
+    <path
+        android:pathData="M 19.436 19.688 L 18.152 19.688 L 18.152 22 L 17 22 L 17 16 L 19.332 16 C 20.097 16 20.687 16.155 21.103 16.466 C 21.519 16.776 21.727 17.225 21.727 17.813 C 21.727 18.214 21.619 18.551 21.403 18.823 C 21.191 19.092 20.893 19.299 20.511 19.445 L 22 21.946 L 22 22 L 20.766 22 L 19.436 19.688 Z M 18.152 18.852 L 19.336 18.852 C 19.725 18.852 20.028 18.764 20.247 18.588 C 20.465 18.409 20.575 18.166 20.575 17.859 C 20.575 17.537 20.473 17.288 20.27 17.113 C 20.069 16.937 19.769 16.846 19.368 16.841 L 18.152 16.841 L 18.152 18.852 Z"
+        android:fillColor="#000000"/>
 </vector>

packages/SystemUI/res/drawable/ic_statusbar_vowifi.xml
@@ -1,0 +1,14 @@
+<vector
+    xmlns:android="http://schemas.android.com/apk/res/android"
+    android:width="24dp"
+    android:height="24dp"
+    android:viewportWidth="24"
+    android:viewportHeight="24"
+    android:tint="?android:attr/colorControlNormal">
+    <path
+        android:pathData="M 21.996 4.95 C 21.787 4.779 19.666 2.999 16.497 2.999 C 13.318 2.999 11.208 4.779 10.999 4.95 L 16.497 11.998 L 21.996 4.95 Z"
+        android:fillColor="#000000"/>
+    <path
+        android:pathData="M 19.997 15.507 C 18.756 15.507 17.548 15.308 16.427 14.938 C 16.077 14.818 15.678 14.907 15.407 15.178 L 13.208 17.377 C 10.378 15.928 8.059 13.618 6.62 10.788 L 8.819 8.589 C 9.098 8.309 9.178 7.919 9.069 7.569 C 8.699 6.45 8.498 5.25 8.498 3.999 C 8.498 3.45 8.049 2.999 7.499 2.999 L 3.999 2.999 C 3.45 2.999 2.999 3.45 2.999 3.999 C 2.999 13.388 10.608 20.997 19.997 20.997 C 20.547 20.997 20.997 20.547 20.997 19.997 L 20.997 16.508 C 20.997 15.957 20.547 15.507 19.997 15.507 Z"
+        android:fillColor="#000000"/>
+</vector>

commit 79103ee48c73e87e6da604af8f3f3f1bbe5c43f8

SystemUI: Fix HD calling icon for RTL layout
The mobile signal icon mirrors itself in RTL mode, so the HD icon
needs to be moved from top left to top right.

Before: https://i.imgur.com/tUGTtC2.jpg
After: https://i.imgur.com/0VWMOrC.jpg

Fixes: 1b894eb5f6 ("SystemUI: Introduce HD & wifi calling statusbar icons")
Change-Id: I20a6c6205c20e189247023b0f4429a27871765dc

packages/SystemUI/res-keyguard/layout/status_bar_mobile_signal_group_inner.xml
@@ -83,9 +83,13 @@
                 />
             <ImageView
                 android:id="@+id/mobile_hd"
-                android:layout_width="wrap_content"
-                android:layout_height="wrap_content"
+                android:layout_width="12dp"
+                android:layout_height="8dp"
+                android:layout_marginTop="1dp"
+                android:layout_gravity="start|top"
                 android:src="@drawable/ic_hd_calling"
+                android:adjustViewBounds="true"
+                android:gravity="start|top"
                 android:visibility="gone" />
         </FrameLayout>
         <ImageView

packages/SystemUI/res/drawable/ic_hd_calling.xml
@@ -1,13 +1,13 @@
 <vector
     xmlns:android="http://schemas.android.com/apk/res/android"
-    android:width="@dimen/signal_icon_size"
-    android:height="@dimen/signal_icon_size"
-    android:viewportWidth="24"
-    android:viewportHeight="24">
+    android:width="12dp"
+    android:height="8dp"
+    android:viewportWidth="12"
+    android:viewportHeight="8">
     <path
-        android:pathData="M 5.4024 8.2 L 4.2252 8.2 L 4.2252 5.0008 L 1.182 5.0008 L 1.182 8.2 L 0 8.2 L 0 1 L 1.182 1 L 1.182 3.9964 L 4.2252 3.9964 L 4.2252 1 L 5.4024 1 L 5.4024 8.2 Z"
+        android:pathData="M 5.402 7.6 L 4.225 7.6 L 4.225 4.401 L 1.182 4.401 L 1.182 7.6 L 0 7.6 L 0 0.4 L 1.182 0.4 L 1.182 3.396 L 4.225 3.396 L 4.225 0.4 L 5.402 0.4 L 5.402 7.6 Z"
         android:fillColor="#000000"/>
     <path
-        android:pathData="M 6.924 8.2 L 6.924 1 L 8.934 1 C 9.535 1 10.068 1.142 10.532 1.425 C 10.999 1.709 11.362 2.111 11.617 2.632 C 11.872 3.153 12 3.749 12 4.422 L 12 4.782 C 12 5.465 11.87 6.065 11.612 6.582 C 11.357 7.101 10.991 7.499 10.513 7.78 C 10.04 8.06 9.497 8.2 8.882 8.2 L 6.924 8.2 Z M 8.107 2.009 L 8.107 7.202 L 8.878 7.202 C 9.498 7.202 9.973 6.996 10.303 6.588 C 10.637 6.176 10.807 5.585 10.813 4.817 L 10.813 4.418 C 10.813 3.635 10.652 3.039 10.332 2.627 C 10.01 2.214 9.545 2.009 8.934 2.009 L 8.107 2.009 Z"
+        android:pathData="M 6.924 7.6 L 6.924 0.4 L 8.934 0.4 C 9.535 0.4 10.068 0.542 10.532 0.825 C 10.999 1.109 11.362 1.511 11.617 2.032 C 11.872 2.553 12 3.149 12 3.822 L 12 4.182 C 12 4.865 11.87 5.465 11.612 5.982 C 11.357 6.501 10.991 6.899 10.513 7.18 C 10.04 7.46 9.497 7.6 8.882 7.6 L 6.924 7.6 Z M 8.107 1.409 L 8.107 6.602 L 8.878 6.602 C 9.498 6.602 9.973 6.396 10.303 5.988 C 10.637 5.576 10.807 4.985 10.813 4.217 L 10.813 3.818 C 10.813 3.035 10.652 2.439 10.332 2.027 C 10.01 1.614 9.545 1.409 8.934 1.409 L 8.107 1.409 Z"
         android:fillColor="#000000"/>
 </vector>

packages/SystemUI/res/values/arrow_dimens.xml
@@ -26,4 +26,7 @@
     <dimen name="bluetooth_dialog_active_device_bottom_margin">4dp</dimen>
     <dimen name="bluetooth_dialog_toggle_height">56dp</dimen>
 
+    <!-- HD calling icon -->
+    <dimen name="signal_icon_viewport_size">24dp</dimen>
+
 </resources>

packages/SystemUI/src/com/android/systemui/statusbar/StatusBarMobileView.java
@@ -135,6 +135,15 @@ public class StatusBarMobileView extends BaseStatusBarFrameLayout implements Dar
         mMobileDrawable = new SignalDrawable(getContext());
         mMobile.setImageDrawable(mMobileDrawable);
 
+        float signalSize = mContext.getResources().getDimensionPixelSize(
+                R.dimen.signal_icon_size);
+        float viewportSize = mContext.getResources().getDimensionPixelSize(
+                R.dimen.signal_icon_viewport_size);
+        LayoutParams lp = (LayoutParams) mMobileHd.getLayoutParams();
+        lp.height = Math.round(lp.height * (signalSize / viewportSize));
+        lp.width = Math.round(lp.width * (signalSize / viewportSize));
+        mMobileHd.setLayoutParams(lp);
+
         initDotView();
     }
 