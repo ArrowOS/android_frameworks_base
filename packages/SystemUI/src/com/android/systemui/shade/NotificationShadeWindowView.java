ea458160719c17171e6c71129cce112a21d698e5
minaripenguin <minaripenguin@users.noreply.github.com>
2023-07-08 20:44:56 +0800

    SystemUI: Re-tune split shade for QPR3
    Change-Id: Ie1e762a8722f46e6f093affb3d1037d6acb4e9d1
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/shade/NotificationShadeWindowView.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationShadeWindowView.java
@@ -94,7 +94,6 @@ public class NotificationShadeWindowView extends FrameLayout {
         if (getFitsSystemWindows()) {
             boolean paddingChanged = insets.top != getPaddingTop()
                     || insets.bottom != getPaddingBottom();
-
             // Drop top inset, and pass through bottom inset.
             if (paddingChanged) {
                 setPadding(0, 0, 0, 0);
@@ -126,9 +125,13 @@ public class NotificationShadeWindowView extends FrameLayout {
             View child = getChildAt(i);
             if (child.getLayoutParams() instanceof LayoutParams) {
                 LayoutParams lp = (LayoutParams) child.getLayoutParams();
-                if (lp.rightMargin != mRightInset || lp.leftMargin != mLeftInset) {
-                    lp.rightMargin = lp.ignoreRightInset ? 0 : mRightInset;
+                boolean marginChanged = lp.rightMargin != mRightInset || lp.leftMargin != mLeftInset;
+                if (marginChanged) {
                     lp.leftMargin = lp.ignoreLeftInset ? 0 : mLeftInset;
+                    lp.rightMargin = lp.ignoreRightInset ? 0 : mRightInset;
+                    if (lp.ignoreLeftInset && lp.rightMargin == 0) {
+                        lp.rightMargin = mRightInset;
+                    }
                     child.requestLayout();
                 }
             }
